66bd186cd4191bc19c94491b0f924ec6fa758a65
Vitor Rodrigues <vitor_rc@outlook.com>
2021-06-30 20:08:06 -0300

    Comitao

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,83 @@
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+from tech_news.analyzer.search_engine import (
+    search_by_category,
+    search_by_date,
+    search_by_source,
+    search_by_title,
+)
+from tech_news.scraper import get_tech_news
+import sys
+
+
+def option_0(option):
+    if option == "0":
+        amount = int(input("Digite a quantidade de noticias: "))
+        print(get_tech_news(amount))
+
+
+def option_1(option):
+    if option == "1":
+        title = input("Digite o titulo: ")
+        print(search_by_title(title))
+
+
+def option_2(option):
+    if option == "2":
+        date = input("Digite a data: ")
+        print(search_by_date(date))
+
+
+def option_3(option):
+    if option == "3":
+        source = input("Digite a fonte: ")
+        print(search_by_source(source))
+
+
+def option_4(option):
+    if option == "4":
+        category = input("Digite a categoria: ")
+        print(search_by_category(category))
+
+
+def option_5(option):
+    if option == "5":
+        print(top_5_news())
+
+
+def option_6(option):
+    if option == "6":
+        print(top_5_categories())
+
+
+def option_7(option):
+    if option == "7":
+        print("Encerrando script")
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    print(
+        "Selecione uma das opções a seguir:\n",
+        "0 - Popular o banco com notícias;\n",
+        "1 - Buscar notícias por título;\n",
+        "2 - Buscar notícias por data;\n",
+        "3 - Buscar notícias por fonte;\n",
+        "4 - Buscar notícias por categoria;\n",
+        "5 - Listar top 5 notícias;\n",
+        "6 - Listar top 5 categorias;\n",
+        "7 - Sair.",
+    )
+
+    option = input()
+
+    if option.isdecimal() and int(option) >= 0 and int(option) <= 7:
+        option_0(option)
+        option_1(option)
+        option_2(option)
+        option_3(option)
+        option_4(option)
+        option_5(option)
+        option_6(option)
+        option_7(option)
+    else:
+        sys.stderr.write("Opção inválida\n")
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,103 @@
+import time
+import requests
+from math import ceil
+from parsel import Selector
+from tech_news.database import create_news
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+
+        if (response.status_code == 200):
+            return response.text
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    new = {}
+    selector = Selector(text=html_content)
+    new['url'] = selector.css("meta[property='og:url']::attr(content)").get()
+    new['title'] = selector.css(".tec--article__header__title::text").get()
+    new['timestamp'] = selector.css("time::attr(datetime)").get()
+    new['writer'] = selector.css(
+        ".tec--author__info__link::text"
+    ).get()
+    if new['writer'] is not None:
+        new['writer'] = new['writer'].strip()
+
+    new['shares_count'] = selector.css(
+        ".tec--toolbar__item::text"
+    ).get()
+
+    if new['shares_count']:
+        new['shares_count'] = int(new['shares_count'].split(' ')[1])
+    else:
+        new['shares_count'] = 0
+
+    new['comments_count'] = selector.css(
+        "#js-comments-btn::attr(data-count)"
+    ).get()
+
+    if new['comments_count']:
+        new['comments_count'] = int(new['comments_count'])
+    else:
+        new['comments_count'] = 0
+
+    new['summary'] = "".join(selector.css(
+        ".tec--article__body p:first-child *::text"
+    ).getall())
+    new['sources'] = list(map(str.strip, selector.css(
+        ".z--mb-16 div a.tec--badge::text"
+    ).getall()))
+    new['categories'] = list(map(str.strip, selector.css(
+        "div#js-categories a.tec--badge::text"
+    ).getall()))
+
+    return new
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(
+      "h3.tec--card__title a.tec--card__title__link::attr(href)"
+    ).getall()
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(
+      "div.tec--list a.tec--btn::attr(href)"
+    ).get()
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    scraped_news = []
+    page = fetch("https://www.tecmundo.com.br/novidades")
+    links_news = scrape_novidades(page)
+
+    total_pages = ceil(amount / 20)
+    print(total_pages)
+
+    if total_pages > 1:
+        for _ in range(1, total_pages):
+            page = scrape_next_page_link(page)
+            next_page_content = fetch(page)
+            next_page_news = scrape_novidades(next_page_content)
+            links_news = links_news + next_page_news
+
+    for link in links_news:
+        news = fetch(link)
+        scraped_news.append(scrape_noticia(news))
+        if len(scraped_news) == amount:
+            break
+    create_news(scraped_news)
+    return scraped_news
