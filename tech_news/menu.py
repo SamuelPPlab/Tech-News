eded3e3a812c323bc8cb4ef2d5f65f51f77ffd44
Lucas <luc.zago@hotmail.com>
2021-07-02 18:38:08 -0300

    corrigida complexidade

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -9,6 +9,24 @@ from tech_news.analyzer.search_engine import (
 from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
+def search(command):
+    if command == 1:
+        search_by_title(str(input("Digite o título: ")))
+    elif command == 2:
+        search_by_date(str(input("Digite a data no formato aaaa-mm-dd: ")))
+    elif command == 3:
+        search_by_source(str(input("Digite a fonte: ")))
+    else:
+        search_by_category(str(input("Digite a categoria: ")))
+
+
+def get_top_news(command):
+    if command == 5:
+        top_5_news()
+    else:
+        top_5_categories()
+
+
 # Requisito 12
 def analyzer_menu():
     """Seu código deve vir aqui"""
@@ -29,18 +47,10 @@ def analyzer_menu():
             get_tech_news(
                 int(input("Digite quantas notícias serão buscadas: "))
             )
-        elif user_entry == 1:
-            search_by_title(str(input("Digite o título: ")))
-        elif user_entry == 2:
-            search_by_date(str(input("Digite a data no formato aaaa-mm-dd: ")))
-        elif user_entry == 3:
-            search_by_source(str(input("Digite a fonte: ")))
-        elif user_entry == 4:
-            search_by_category(str(input("Digite a categoria: ")))
-        elif user_entry == 5:
-            top_5_news()
-        elif user_entry == 6:
-            top_5_categories()
+        elif 0 < user_entry < 5:
+            search(user_entry)
+        elif 4 < user_entry < 7:
+            get_top_news(user_entry)
         else:
             print("Encerrando script")
     except ValueError:
