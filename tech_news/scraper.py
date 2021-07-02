4a7e3212ac7e3e6ce942ca5986d62a9471dff080
Luise Rios <luisemiranda@gmail.com>
2021-07-02 14:47:42 -0300

    requisito 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
+from math import ceil
 
 
 # Requisito 1
@@ -19,33 +21,51 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
-    result = {}
     selector = Selector(text=html_content)
-    result["url"] = selector.css(
-        "meta[property='og:url']::attr(content)"
-    ).get()
-    result["title"] = selector.css("#js-article-title::text").get()
-    result["timestamp"] = selector.css(
-        "#js-article-date::attr(datetime)"
-    ).get()
-    result["writer"] = (
-        selector.css(".tec--author__info__link::text").get()[1:-1] or None
-    )
-    result["shares_count"] = (
-        int(selector.css(".tec--toolbar__item::text").get()[1]) or 0
-    )
-    result["comments_count"] = int(
-        selector.css("#js-comments-btn::attr(data-count)").get()
-    )
-    result["summary"] = "".join(
+
+    url = selector.css("meta[property='og:url']::attr(content)").get()
+
+    title = selector.css("#js-article-title::text").get()
+
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+
+    writer = selector.css(".tec--author__info__link::text").get()
+    if writer:
+        writer = writer.strip()
+
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    if shares_count:
+        shares_count = int((shares_count.strip()).split(" ")[0])
+    else:
+        shares_count = 0
+
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    if comments_count:
+        comments_count = int(comments_count)
+    else:
+        comments_count = 0
+
+    summary = "".join(
         selector.css(".tec--article__body > p:first-child *::text").getall()
     )
-    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
-    result["sources"] = list(map(str.strip, sources))
-    categories = selector.css("#js-categories .tec--badge::text").getall()
-    result["categories"] = list(map(str.strip, categories))
 
-    return result
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    sources = [source.strip() for source in sources]
+
+    categories = selector.css("#js-categories a::text").getall()
+    categories = [category.strip() for category in categories]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 3
@@ -65,3 +85,26 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    content = fetch(url)
+    news_links = scrape_novidades(content)
+    news = []
+
+    for link in news_links:
+        link_content = fetch(link)
+        new = scrape_noticia(link_content)
+        news.append(new)
+
+    pages = ceil(amount / len(news_links))
+    if pages > 1:
+        for _ in range(1, pages):
+            content = fetch(url)
+            url = scrape_next_page_link(content)
+            content = fetch(url)
+            news_links = scrape_novidades(content)
+            for link in news_links:
+                link_content = fetch(link)
+                new = scrape_noticia(link_content)
+                news.append(new)
+    create_news(news[:amount])
+    return news[:amount]
