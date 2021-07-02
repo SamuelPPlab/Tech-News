d99087e923a70b155d0fdadb41197244ed01afce
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-01 21:15:17 -0300

    feat: news scrape

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import pprint
 from parsel import Selector
 # Requisito 1
 def fetch(url):
@@ -18,7 +19,29 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    
+    data = {}
+    selector = Selector(html_content)
+    data['url'] = selector.css('head link[href*="tecmundo.com.br/"]::attr(href)').get()
+    data['title'] = selector.css('#js-article-title ::text').get()
+    data['timestamp'] = selector.css('#js-article-date ::attr(datetime)').get()
+    # 
+    data['writer'] = selector.css('.tec--author__info__link ::text').get().strip()
+    data['shares_count'] = int(selector.css('.tec--toolbar__item ::text').get().strip('Compartilharam'))
+    data['comments_count'] = int(selector.css('#js-comments-btn ::attr(data-count)').get())
+    data['summary'] = ''.join(selector.css('.tec--article__body > p:first-child ::text').getall())
+    # 
+    data['sources'] = [item.strip(' ') for item in 
+        list(filter(lambda x: x != " ", selector.css('.z--mb-16 .tec--badge ::text').getall()
+    ))]
+    # 
+    data['categories'] = [item.strip(' ') for item in
+        filter(lambda x: x != " ",
+            list(selector.css('.z--px-16 #js-categories ::text').getall()
+    ))]
+
+
+    return(data)
+
 
 
 # Requisito 3
@@ -34,3 +57,10 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+fetched_data = fetch('https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm')
+
+pp.pprint(scrape_noticia(fetched_data))
