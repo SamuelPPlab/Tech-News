4b9b9af4fb09c26da3164fb5cf363a77d8332f81
julioKauer <juliokauer@gmail.com>
2021-07-02 23:38:03 -0300

    Requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,48 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    noticia = {}
+
+    noticia['url'] = selector.css(
+        "link[rel='canonical']::attr(href)"
+    ).get()
+
+    noticia['title'] = selector.css(
+        "h1.tec--article__header__title::text"
+    ).get()
+
+    noticia['timestamp'] = selector.css("time::attr(datetime)").get()
+
+    noticia['writer'] = selector.css(
+        "a.tec--author__info__link::text"
+    ).get().strip()
+
+    noticia['shares_count'] = selector.css(
+        "div.tec--toolbar__item::text"
+    ).get()
+    suffix = " Compartilharam"
+    if noticia['shares_count'].endswith(suffix):
+        noticia['shares_count'] = noticia['shares_count'][1:-len(suffix)]
+    noticia['shares_count'] = int(noticia['shares_count'])
+
+    noticia['comments_count'] = selector.css(
+        "button#js-comments-btn::attr(data-count)"
+    ).get()
+    noticia['comments_count'] = int(noticia['comments_count'])
+    noticia["summary"] = "".join(
+        selector.css("div.tec--article__body > p:first-child *::text").getall()
+    )
+
+    noticia["sources"] = list(map(
+        str.strip, selector.css(".z--mb-16 div a.tec--badge::text").getall()
+    ))
+
+    noticia["categories"] = list(map(
+        str.strip, selector.css("div#js-categories a::text").getall()
+    ))
+
+    return noticia
 
 
 # Requisito 3
