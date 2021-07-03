b7b230eab8a648def4e185655a82b515a31333df
danielmlduarte <danielduarte.tryber@gmail.com>
2021-07-03 12:19:20 -0300

    Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+    except requests.ReadTimeout:
+        None
+    finally:
+        if response.status_code == "200":
+            print(response)
+        else:
+            None
 
 
 # Requisito 2
