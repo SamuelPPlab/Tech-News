2db5a12eb78f816ea4d02a260469ef1c1ea82f3f
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-07-04 00:46:06 -0300

    requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+import parsel
 
 # Requisito 1
 def fetch(url):
@@ -19,8 +20,22 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    scrape_info = {}
 
+    selector = parsel.Selector(html_content)
+
+    scrape_info['url'] = selector.css("link[rel=canonical]::attr(href)").get()
+    scrape_info['title'] = selector.css("h1::text").get()
+    scrape_info['timestamp'] = selector.css("time::attr(datetime)").get()
+    scrape_info['writer'] = selector.css(".tec--author__info__link::text").get().strip() if selector.css(".tec--author__info__link::text").get() else None
+    scrape_info['shares_count'] = selector.css(".tec-toolbar__item::text").strip().split(' ')[0].get() if selector.css(".tec-toolbar__item::text") else 0
+    scrape_info['comments_count'] = int(selector.css("button#js-comments-btn::attr(data-count)").get())
+    scrape_info['summary'] = "".join(selector.css(".tec--article__body p:first-child *::text").getall())
+    scrape_info['sources'] = [font.strip() for font in selector.css("h2:contains('Fontes') + div a::text").getall()]
+    scrape_info['categories'] = [category.strip() for category in selector.css("div#js-categories a::text").getall()]
+    
+
+    return scrape_info
 
 # Requisito 3
 def scrape_novidades(html_content):
