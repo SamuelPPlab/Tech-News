366ef6a060dca9f5854287f2c4484a3aebfba39f
dennerpl <denner.souzapl@gmail.com>
2021-07-04 20:37:51 -0300

    requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -48,7 +48,14 @@ def scrape_noticia(html_content):
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    list = []
+    if selector.css("h3 a.tec--card__title__link"):
+        list = selector.css("h3 a.tec--card__title__link::attr(href)").getall()
+    else:
+        list = []
+    return list
+
 
 
 # Requisito 4
