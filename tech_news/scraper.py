9f67bbc4a820706a1e0a3e6fdd214a66e9d199a3
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-07-04 01:05:23 -0300

    corrigindo lint

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import requests
 from time import sleep
 import parsel
 
+
 # Requisito 1
 def fetch(url):
     sleep(1)
@@ -27,15 +28,33 @@ def scrape_noticia(html_content):
     scrape_info['url'] = selector.css("link[rel=canonical]::attr(href)").get()
     scrape_info['title'] = selector.css("h1::text").get()
     scrape_info['timestamp'] = selector.css("time::attr(datetime)").get()
-    scrape_info['writer'] = selector.css(".tec--author__info__link::text").get().strip() if selector.css(".tec--author__info__link::text").get() else None
-    scrape_info['shares_count'] = selector.css(".tec-toolbar__item::text").strip().split(' ')[0].get() if selector.css(".tec-toolbar__item::text") else 0
-    scrape_info['comments_count'] = int(selector.css("button#js-comments-btn::attr(data-count)").get())
-    scrape_info['summary'] = "".join(selector.css(".tec--article__body p:first-child *::text").getall())
-    scrape_info['sources'] = [font.strip() for font in selector.css("h2:contains('Fontes') + div a::text").getall()]
-    scrape_info['categories'] = [category.strip() for category in selector.css("div#js-categories a::text").getall()]
-    
+    scrape_info['writer'] = (
+        selector.css(".tec--author__info__link::text").get().strip()
+        if selector.css(".tec--author__info__link::text").get() else None
+    )
+    scrape_info['shares_count'] = (
+        selector.css(".tec-toolbar__item::text").strip().split(' ')[0].get()
+        if selector.css(".tec-toolbar__item::text") else 0
+    )
+    scrape_info['comments_count'] = int(
+        selector.css("button#js-comments-btn::attr(data-count)").get()
+    )
+    scrape_info['summary'] = "".join(
+        selector.css(".tec--article__body p:first-child *::text").getall()
+    )
+    scrape_info['sources'] = [
+        font.strip() for font in selector.css(
+            "h2:contains('Fontes') + div a::text"
+        ).getall()
+    ]
+    scrape_info['categories'] = [
+        category.strip() for category in selector.css(
+            "div#js-categories a::text"
+        ).getall()
+    ]
 
     return scrape_info
+
 
 # Requisito 3
 def scrape_novidades(html_content):
