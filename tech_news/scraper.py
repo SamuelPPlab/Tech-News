7c4a7629b7096a47b216dffebb6ae7798f99a925
Carlos Souza <carlos.pessoal@hotmail.com>
2021-07-04 16:59:01 -0300

    Quarto requisito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -44,13 +44,13 @@ def scrape_noticia(html_content):
 # Requisito 3
 def scrape_novidades(html_content):
     selector = Selector(html_content)
-    links = selector.css('.tec--list__item .tec--card__title__link::attr(href)').getall()
-    return links
+    return selector.css('.tec--list__item .tec--card__title__link::attr(href)').getall()
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css('.tec--list > a::attr(href)').get()
 
 
 # Requisito 5
