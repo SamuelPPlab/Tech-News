edbe2356b90107d71879c996c5d1126729e6afcf
dennerpl <denner.souzapl@gmail.com>
2021-07-04 20:58:17 -0300

    arrumando linter

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -23,20 +23,30 @@ def scrape_noticia(html_content):
     selector = Selector(text=html_content)
     result["url"] = selector.css("link[rel=canonical]::attr(href)").get()
     result["title"] = selector.css("h1#js-article-title::text").get().strip()
-    result["timestamp"] = selector.css("time#js-article-date::attr(datetime)").get()
+    result["timestamp"] = selector.css(
+        "time#js-article-date::attr(datetime)"
+    ).get()
     if selector.css("a.tec--author__info__link::text"):
-        result["writer"] = selector.css("a.tec--author__info__link::text").get().strip()
+        result["writer"] = selector.css(
+            "a.tec--author__info__link::text"
+        ).get().strip()
     else:
         result["writer"] = None
     if selector.css("div.tec--toolbar__item::text"):
-        result["shares_count"] = int(selector.css("div.tec--toolbar__item::text").get().strip()[0])
+        result["shares_count"] = int(selector.css(
+            "div.tec--toolbar__item::text"
+        ).get().strip()[0])
     else:
         result["shares_count"] = 0
     if selector.css("button#js-comments-btn::text"):
-        result["comments_count"] = int(selector.css("button#js-comments-btn::attr(data-count)").get())
+        result["comments_count"] = int(selector.css(
+            "button#js-comments-btn::attr(data-count)"
+        ).get())
     else:
         result["comments_count"] = 0
-    result["summary"] = "".join(selector.css("div.tec--article__body p:first-child *::text").getall())
+    result["summary"] = "".join(selector.css(
+        "div.tec--article__body p:first-child *::text"
+        ).getall())
     result["sources"] = []
     for source in selector.css("div.z--mb-16 a.tec--badge::text").getall():
         result["sources"].append(source.strip())
@@ -57,10 +67,15 @@ def scrape_novidades(html_content):
     return list
 
 
-
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    result = None
+    if selector.css("a.z--mx-auto"):
+        result = selector.css("a.z--mx-auto::attr(href)").get()
+        return result
+    else:
+        return result
 
 
 # Requisito 5
