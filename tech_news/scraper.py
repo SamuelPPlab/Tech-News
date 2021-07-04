d7c7adcbc221485cd4892136da58a4a62421a7e7
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-07-03 23:13:49 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+from time import sleep
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    sleep(1)
+
+    try:
+        response = requests.get(url, timeout=3)
+
+        if response.status_code == 200:
+            return response.text
+
+        return None
+
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
@@ -21,3 +35,5 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+# print(fetch('https://www.tecmundo.com.br/novidades'))
