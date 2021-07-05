e296bde5ef0305e55c94133b29d91eca6a8fbed3
Carlos Souza <carlos.pessoal@hotmail.com>
2021-07-04 22:31:04 -0300

    Sétimo requisito

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,23 @@
+import re
 from tech_news.database import search_news
+
+def format_document(dict):
+    return (dict['title'], dict['url'])
 
 # Requisito 6
 def search_by_title(title):
     results = search_news({ 'title': { '$regex': title, '$options': 'i' } })
-    return [(news['title'], news['url']) for news in results]
+    return [format_document(news) for news in results]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    date_regex = r'^20\d{2}-(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[01])$'
+    if (re.search(date_regex, date) is None):
+        raise ValueError("Data inválida")
+
+    results = search_news({ 'timestamp': { '$regex': date } })
+    return [format_document(news) for news in results]
 
 
 # Requisito 8
