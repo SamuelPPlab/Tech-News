74cd951dffa93bbd54b470da32ec0dec2c633ac7
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-06 11:47:47 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,25 @@
 # Requisito 1
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+from requests.exceptions import ReadTimeout
+from tech_news.database import create_news
+
+
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        sleep(1)
+        response = requests.get(url, timeout=3)
+        return None if response.status_code != 200 else response.text
+    except ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    
 
 
 # Requisito 3
