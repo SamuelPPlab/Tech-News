8b930f1e1513497f32df8bb00ccb2ac4f42f1fc5
CleberFontinele <cleberfontinele2387@gmail.com>
2021-07-06 19:44:33 -0300

    requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,9 +18,51 @@ def fetch(url):
 
 
 # Requisito 2
+# Referencias: Instrutores Tulio Olivieri e Maria Carolina
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    data = {}
+    selector = Selector(text=html_content)
 
+    data["url"] = selector.css(
+        "head > meta[property='og:url']::attr(content)").get()
+
+    data["title"] = selector.css("#js-article-title::text").get()
+
+    data["timestamp"] = selector.css("time::attr(datetime)").get()
+
+    if selector.css(".tec--author__info__link::text"):
+        data["writer"] = selector.css(
+            ".tec--author__info__link::text").get().strip()
+    else:
+        data["writer"] = None
+    # http://devfuria.com.br/python/strings/
+    # Método strip() Retira espaços em branco no começo e no fim
+
+    if selector.css(".tec--toolbar__item::text"):
+        data["shares_count"] = int(selector.css(
+            ".tec--toolbar__item::text").get().split()[0])
+    else:
+        data["shares_count"] = 0
+
+    data["comments_count"] = int(selector.css(
+        "#js-comments-btn::attr(data-count)").get())
+
+    data["summary"] = "".join(
+        selector.css(".tec--article__body p:first-child *::text").getall()
+    )
+
+    # data["sources"] = []
+    # for source in selector.css(".z--mb-16 div a.tec--badge::text").getall():
+    #     data["sources"].append(source.strip())
+
+    # Referencia: Estruturas de repetição, compreensão de listas, 34.1
+    data["sources"] = [source.strip() for source in selector.css(
+        ".z--mb-16 div a.tec--badge::text").getall()]
+
+    data["categories"] = [category.strip() for category in selector.css(
+        "#js-categories a.tec--badge::text").getall()]
+    return data
+   
 
 # Requisito 3
 def scrape_novidades(html_content):
@@ -34,3 +77,11 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# if __name__ == "__main__":
+
+#     html_content = fetch(
+#         'https://www.tecmundo.com.br/minha-serie/215330-8-series-parecidas-the-crown-fas-realeza.htm'
+#     )
+#     print(scrape_noticia(html_content))
