e813c90039e9280c5b49b8a529451048d713009d
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-06 14:33:12 -0300

    requisito6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,32 @@
 # Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    from tech_news.database import search_news
+    from datetime import datetime
+
+def format_title_url(news_list):
+    return [(news(["title"], news["url"])) for news in news_list]
+
+
+def search_by_title(title):
+    query = {"title": {"$regex": title, "$options": "i"}}
+    news_list = search_news(query)
+    return format_title_url(news_list)
+
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    
