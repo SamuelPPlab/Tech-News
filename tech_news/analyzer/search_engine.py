6fbb6d22034b915197e120dae57b061a092c6456
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-05 23:16:46 -0300

    Requisitos 6 e 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,36 @@
+from tech_news.database import search_news
+import datetime
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    results = search_news({})
+    result_tupla = []
+    for result in results:
+        if title.upper() == result["title"].upper():
+            tupla = (result["title"], result["url"])
+            result_tupla.append(tupla)
+
+    return result_tupla
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+        results = search_news({})
+        result_tupla = []
+        for result in results:
+            time_list = result["timestamp"].split("T")
+            if time_list[0] == date:
+                tupla = (result["title"], result["url"])
+                result_tupla.append(tupla)
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    return result_tupla
 
 
 # Requisito 8
