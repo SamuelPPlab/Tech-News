b88204e26e0d5df9547d0ec48360a558a0ef0964
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-06 18:03:49 -0300

    req 10 com ambiente virtual

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    popularity_list = sorted(
+        news_list,
+        key=lambda x: x["shares_count"] + x["comments_count"],
+        reverse=True,
+    )
+    return ([(news["title"], news["url"]) for news in popularity_list[:5]],)
 
 
 # Requisito 11
