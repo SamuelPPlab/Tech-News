21b5ff321e397a365b52c582e091ec50604b4852
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-07-06 18:11:42 -0300

    requisito 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,9 @@
 import parsel
+import pymongo
 import requests
 import time
 import re
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -16,29 +18,45 @@ def fetch(url):
 
 
 # Requisito 2
-def scrape_noticia(html_content: str):
+def scrape_noticia(html_content):
     selector = parsel.Selector(html_content)
     selector = selector.css(".tec--article")
 
-    start_index = html_content.find(""""@id":""")
+    start_index = html_content.find("@id")
     end_index = html_content.find("}}", start_index)
-    url = html_content[(start_index + 7):(end_index - 1)]
+    url = html_content[(start_index + 6):(end_index - 1)]
 
     title = selector.css("#js-article-title::text").get()
+    if title:
+        title = title.strip()
+    else:
+        title = None
 
     timestamp = selector.css("time#js-article-date::attr(datetime)").get()
 
     writer = selector.css("a.tec--author__info__link::text").get()
-    writer = writer.strip()
+    if writer:
+        writer = writer.strip()
+    else:
+        writer = None
 
     shares_count = selector.css("div.tec--toolbar__item::text").re_first(r"\d")
-    shares_count = int(shares_count)
+    if shares_count:
+        shares_count = int(shares_count)
+    else:
+        shares_count = 0
 
     comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
-    comments_count = int(comments_count)
+    if comments_count:
+        comments_count = int(comments_count)
+    else:
+        comments_count = 0
 
     summary = selector.css(".tec--article__body p:first-child").get()
-    summary = re.sub(r"<[^>]*>", "", summary)
+    if summary:
+        summary = re.sub(r"<[^>]*>", "", summary)
+    else:
+        summary = ""
 
     sources = selector.css("div > .tec--badge[target=_blank]::text").getall()
     sources = [source.strip() for source in sources]
@@ -70,7 +88,12 @@ def scrape_novidades(html_content):
         + "a.tec--card__thumb__link::attr(href)"
     ).getall()
 
-    return links
+    filtered_links = []
+    for link in links:
+        if link not in filtered_links:
+            filtered_links.append(link)
+
+    return filtered_links
 
 
 # Requisito 4
@@ -85,4 +108,35 @@ def scrape_next_page_link(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    news_page_url = "https://www.tecmundo.com.br/novidades"
+    news_counter = 0
+    news_list_length = 0
+    news_list = []
+
+    while news_counter < amount:
+        if news_list_length == 0 and len(news_list) > 0:
+            news_page_url = scrape_next_page_link(news_page_url)
+
+        html = fetch(news_page_url)
+
+        news_url_list = scrape_novidades(html)
+        news_list_length = len(news_list)
+
+        for news_url in news_url_list:
+            html = fetch(news_url)
+            news = scrape_noticia(html)
+
+            news_list.append(news)
+
+            news_counter += 1
+            news_list_length -= 1
+
+            if news_counter == amount:
+                break
+
+    try:
+        create_news(news_list)
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print("Error ao cadastrar os dados!")
+    finally:
+        return news_list
