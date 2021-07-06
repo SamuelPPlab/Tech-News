f3a5b6e4d9bb6a5ce7602eb06bf0a96e8ac10dcd
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-06 16:39:28 -0300

    requisitos 6 a 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -3,11 +3,6 @@ from datetime import datetime
 from tech_news.database import search_news
 
 
-def search_by_title(title):
-    from tech_news.database import search_news
-    from datetime import datetime
-
-
 def format_title_url(news_list):
     return [(news(["title"], news["url"])) for news in news_list]
 
@@ -25,15 +20,19 @@ def search_by_date(date):
         query = {"timestamp": {"$regex": date}}
         news_date = search_news(query)
         return format_title_url(news_date)
-    except: ValueError:
+    except ValueError:
         raise ValueError("Data Inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    
+    query = {"sources": {"$regex": source, "$options": "i"}}
+    sources_list = search_news(query)
+    return format_title_url(sources_list)
 
 
 # Requisito 9
 def search_by_category(category):
-    
+    query = {"categories": {"$regex": category, "$options": "i"}}
+    categories_list = search_news(query)
+    return format_title_url(categories_list)
