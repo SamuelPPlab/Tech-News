0f4922b23bf9248efcff7fae1948fb7711c11825
jr3564 <rodolfojr94@gmail.com>
2021-07-06 17:58:18 -0300

    fetch function implemented

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,16 @@
+import requests
+import time
+
 # Requisito 1
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        return response.text if response.status_code == 200 else None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
