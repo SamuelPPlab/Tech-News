ca3d3b47fbbe21a4ac989c125c91e09293b8b644
wberilo <wberilo@gmail.com>
2021-07-06 18:00:25 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
+def format_document(dict):
+    return (dict['title'], dict['url'])
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    results = search_news({'title': {'$regex': title, '$options': 'i'}})
+    return [format_document(news) for news in results]
 
 
 # Requisito 7
