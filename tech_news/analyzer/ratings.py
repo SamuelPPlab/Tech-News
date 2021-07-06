8d5cda9640783615e9748b3302377afbe5fb8ecf
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-06 15:50:17 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,42 @@
+from tech_news.database import search_news
+
+
 # Requisito 10
+def put_popularity(results):
+    for result in results:
+        popularity = result["shares_count"] + result["comments_count"]
+        result["popularity"] = popularity
+    return results
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    results = search_news({})
+
+    results = put_popularity(results)
+    # for result in results:
+    #     popularity = result["shares_count"] + result["comments_count"]
+    #     result["popularity"] = popularity
+
+    results.sort(key=lambda x: x["title"])
+
+    def get_my_pop_key(obj):
+        return obj["popularity"]
+
+    results.sort(key=get_my_pop_key, reverse=True)
+
+    result_tupla = []
+    if len(results) <= 5:
+        for result in results:
+            tupla = (result["title"], result["url"])
+            result_tupla.append(tupla)
+        return result_tupla
+
+    for i in range(5):
+        tupla = (results[i]["title"], results[i]["url"])
+        result_tupla.append(tupla)
+
+    return result_tupla
 
 
 # Requisito 11
