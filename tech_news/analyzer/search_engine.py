b6e4c05c8291f0995234d0e250ae5976135d1e8b
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-05 23:51:01 -0300

    Requisitos 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -36,8 +36,30 @@ def search_by_date(date):
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    results = search_news({})
+    result_tupla = []
+    for result in results:
+        upper_sources_list = []
+        for a_source in result["sources"]:
+            upper_sources_list.append(a_source.upper())
+        if source.upper() in upper_sources_list:
+            tupla = (result["title"], result["url"])
+            result_tupla.append(tupla)
+
+    return result_tupla
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    results = search_news({})
+    result_tupla = []
+    for result in results:
+        upper_categories_list = []
+        for a_category in result["categories"]:
+            upper_categories_list.append(a_category.upper())
+        if category.upper() in upper_categories_list:
+            tupla = (result["title"], result["url"])
+            result_tupla.append(tupla)
+
+    return result_tupla
