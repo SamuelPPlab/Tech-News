27a2d4387c8b41c26abb487306de948f526f614e
Francisco <franciscoliratorres@gmail.com>
2021-07-06 14:25:36 -0300

    add req1

---
+++ tech_news/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+# Requisito 2
+# def scrape_noticia(html_content):
+#     """Seu código deve vir aqui"""
+
+
+# # Requisito 3
+# def scrape_novidades(html_content):
+#     """Seu código deve vir aqui"""
+
+
+# # Requisito 4
+# def scrape_next_page_link(html_content):
+#     """Seu código deve vir aqui"""
+
+
+# # Requisito 5
+# def get_tech_news(amount):
+#     """Seu código deve vir aqui"""
