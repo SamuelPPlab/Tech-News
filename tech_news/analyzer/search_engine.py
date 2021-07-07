d4e81eafe56d2e3e3c33e43f639eeaea4ade27a0
nevesfreire <nando.neves@gmail.com>
2021-07-07 18:18:19 -0300

    7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import get_collection, search_news
+import re
 
 
 # Requisito 6
@@ -14,19 +15,24 @@ def search_by_title(title):
     return result
 
 
-# search_by_title('elon')
-
-
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
-    found = get_collection().find({"datetime": date})
-    output = []
-    for item in list(found):
-        print('entrou')
-        output.append((item.title, item.url))
-    print("output", output)
-    return output
+    test = re.search(
+        "^20[0-2][0-9]-((0[1-9])|(1[0-2]))-([0-2][1-9]|3[0-1])$", date)
+    if not test:
+        raise ValueError('Data inválida')
+    else:
+        # found = list(get_collection().find({"timestamp": date}))
+        found = get_collection().find(
+            {"timestamp": {"$regex": date, "$options": "-i"}})
+        print('found', found)
+        output = []
+        for item in found:
+            print('entrou')
+            output.append((item["title"], item["url"]))
+        # print("output", output)
+        return output
 
 
 # Requisito 8
