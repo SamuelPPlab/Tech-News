a71033fc5d3e8540bc662c380008ae714d7f6c85
Vanessa Oliveira <vanessanaara@gmail.com>
2021-07-07 15:49:18 -0300

    Requisitos 1 e 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,42 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+    except requests.ReadTimeout:
+        return None
+    if response.status_code != 200:
+        return None
+    return response.text
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    share_count = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    comments_count = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    summary = selector.css(
+        ".tec--article__body > p:first-child *::text"
+    ).getall()
+    sources = selector.css("div.z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("div#js-categories a.tec--badge::text").getall()
+    newsInfo = {
+        "url": selector.css("meta[property='og:url']::attr(content)").get(),
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
+        "writer": selector.css(".tec--author__info__link::text").get().strip(),
+        "shares_count": int(share_count),
+        "comments_count": int(comments_count),
+        "summary": "".join(summary),
+        "sources": list(map(str.strip, sources)),
+        "categories": list(map(str.strip, categories))
+    }
+    return newsInfo
 
 
 # Requisito 3
