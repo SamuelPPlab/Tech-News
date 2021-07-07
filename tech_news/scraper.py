dd17220152bb7b0b8d67e18bf1add7aae9f20afc
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-07-07 18:42:36 -0300

    Finalizando requisito 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 import requests
 import time
+import math
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -22,22 +24,33 @@ def scrape_noticia(html_content):
     selector = Selector(html_content)
     shares_count = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
     comments_count = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    writer = selector.css(".tec--author__info__link::text").get()
     dictionary = {
         "url": selector.css("meta[property='og:url']::attr(content)").get(),
         "title": selector.css(".tec--article__header__title::text").get(),
         "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
-        "writer": selector.css(".tec--author__info__link::text").get().strip(),
-        "shares_count": int(shares_count),
-        "comments_count": int(comments_count),
-        "categories": list(map(
-            str.strip, selector.css("div#js-categories a.tec--badge::text")
-            .getall())),
-        "sources": list(map(
-            str.strip, selector.css("div.z--mb-16 .tec--badge::text")
-            .getall())),
-        "summary": "".join(selector.css(
-         ".tec--article__body > p:first-child *::text"
-        ).getall())
+        "writer": writer.strip() if writer else writer,
+        "shares_count": int(shares_count) if shares_count else 0,
+        "comments_count": int(comments_count)
+        if comments_count
+        else comments_count,
+        "categories": list(
+            map(
+                str.strip,
+                selector.css("div#js-categories a.tec--badge::text").getall(),
+            )
+        ),
+        "sources": list(
+            map(
+                str.strip,
+                selector.css("div.z--mb-16 .tec--badge::text").getall(),
+            )
+        ),
+        "summary": "".join(
+            selector.css(
+                ".tec--article__body > p:first-child *::text"
+            ).getall()
+        ),
     }
     return dictionary
 
@@ -51,9 +64,23 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css(".tec--btn::attr(href)").get()
+    return url
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    noticias = []
+    url_list = []
+    url = "https://www.tecmundo.com.br/novidades"
+    for _ in range(math.ceil(amount / 20)):
+        data = fetch(url)
+        url_novidades = scrape_novidades(data)
+        for url_item in url_novidades:
+            url_list.append(url_item)
+        url = scrape_next_page_link(data)
+    for index in range(amount):
+        noticias.append(scrape_noticia(fetch(url_list[index])))
+    create_news(noticias)
+    return noticias
