8bedef45edd61ac29be8852c3e7ae3b169d1445d
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-07-07 16:16:07 -0300

    Requisito 1, 2, 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,52 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+    except requests.ReadTimeout:
+        return None
+    if response.status_code != 200:
+        return None
+    return response.text
 
 
+# Chave summary foi montada consultando o pr do vitor-rc1
+# https://github.com/tryber/sd-07-tech-news/pull/1/files
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    shares_count = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    comments_count = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    dictionary = {
+        "url": selector.css("meta[property='og:url']::attr(content)").get(),
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
+        "writer": selector.css(".tec--author__info__link::text").get().strip(),
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "categories": list(map(
+            str.strip, selector.css("div#js-categories a.tec--badge::text")
+            .getall())),
+        "sources": list(map(
+            str.strip, selector.css("div.z--mb-16 .tec--badge::text")
+            .getall())),
+        "summary": "".join(selector.css(
+         ".tec--article__body > p:first-child *::text"
+        ).getall())
+    }
+    return dictionary
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("h3 > a.tec--card__title__link::attr(href)").getall()
+    return url
 
 
 # Requisito 4
