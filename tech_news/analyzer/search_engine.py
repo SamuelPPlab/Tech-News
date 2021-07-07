8fddf78f12ed147ce7d2d8836c44762e267e638c
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-07 18:17:09 -0300

    feat: seach news by title in db

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
+from tech_news.database import get_collection
+import pprint
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    title_query = {'title': {'$regex': title, '$options': '-i'}}
+    query_filter = {'title': 1, 'url': 1, '_id': 0}
+    info = list(get_collection().find(title_query, query_filter))
+    resp_values = list()
+    for item in info:
+        resp_values.append((item['title'], item['url']))
+    return resp_values
 
 
 # Requisito 7
@@ -16,3 +29,7 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# title_search = search_by_title('é')
+# print(type(title_search))
