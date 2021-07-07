b617e0edda2e3627a826a854572d184562615e2f
Carol Bezerra <carol.arbe@gmail.com>
2021-07-07 16:12:09 -0300

    Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+
+    time.sleep(1)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
 
 
 # Requisito 2
