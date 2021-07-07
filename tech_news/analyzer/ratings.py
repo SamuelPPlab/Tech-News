1da67b0ffa583a62cbd3e092b1a20bb0bdfb249a
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-07-06 21:58:23 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,34 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    found_news = find_news()
+
+    news_list = []
+    for news in found_news:
+        title = news.get("title")
+        url = news.get("url")
+        comments_count = news.get("comments_count")
+        shares_count = news.get("shares_count")
+        popularity = comments_count + shares_count
+
+        news_list.append((title, url, popularity))
+
+    def sort_by_popularity(news):
+        (title, url, popularity) = news
+        return popularity
+
+    news_list.sort(key=sort_by_popularity, reverse=True)
+
+    filtered_list = []
+    counter = 0
+    while counter < 5 and counter < len(news_list):
+        (title, url, popularity) = news_list[counter]
+        filtered_list.append((title, url))
+        counter += 1
+
+    return filtered_list
 
 
 # Requisito 11
