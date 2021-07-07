f3c1e2ca3de23a7922b52b1ea12987db7afcbd7f
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-07-07 18:13:10 -0300

    Req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,10 +1,30 @@
+from pymongo import MongoClient
+from decouple import config
+
+DB_HOST = config("DB_HOST", default="localhost")
+DB_PORT = config("DB_PORT", default="27017")
+
+client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+db = client.tech_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    lista_tupla = []
+    lista = list(
+        db.news.find(
+            {"title": {"$regex": title, "$options": "-i"}},
+            {"_id": 0, "title": 1, "url": 1},
+        )
+    )
+    for i in lista:
+        lista_tupla.append((i["title"], i["url"]))
+    return lista_tupla
 
 
 # Requisito 7
 def search_by_date(date):
+    
     """Seu código deve vir aqui"""
 
 
