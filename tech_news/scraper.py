0e5a42db8c509e77e18cefc24ebd6f67b001e714
Berchon <berchon@gmail.com>
2021-07-07 18:42:16 -0300

    Requisitos 1, 2, 3, 4 e 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,9 @@
-from parsel import Selector
 import requests
 import time
+import math
+from parsel import Selector
+
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -14,10 +17,6 @@ def fetch(url):
         if response.status_code == 200:
             return response.text
         return None
-
-
-def aaa():
-    return "ab"
 
 
 # Requisito 2
@@ -43,10 +42,8 @@ def scrape_noticia(html_content):
         "title": title,
         "timestamp": timestamp,
         "writer": writer.strip() if writer else writer,
-        "shares_count": (int(shares_count) if shares_count else shares_count),
-        "comments_count": (
-            int(comments_count) if comments_count else comments_count
-        ),
+        "shares_count": (int(shares_count) if shares_count else 0),
+        "comments_count": (int(comments_count) if comments_count else 0),
         "summary": summary,
         "sources": [source.strip() for source in sources]
         if sources
@@ -59,26 +56,30 @@ def scrape_noticia(html_content):
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
-    # tec--card__title__link
     selector = Selector(text=html_content)
     return selector.css("h3 > a.tec--card__title__link::attr(href)").getall()
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css("a.tec--btn::attr(href)").get()
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    noticias = []
+    url_list = []
+    url = "https://www.tecmundo.com.br/novidades"
+    for _ in range(math.ceil(amount / 20)):
+        data = fetch(url)
+        url_novidades = scrape_novidades(data)
+        for url_item in url_novidades:
+            url_list.append(url_item)
+        url = scrape_next_page_link(data)
 
+    for index in range(amount):
+        noticias.append(scrape_noticia(fetch(url_list[index])))
 
-# print(
-#     scrape_noticia(
-#         fetch(
-#             "https://www.tecmundo.com.br/minha-serie/220561-wandavision-roteiristas-nao-entenderam-trama-serie-marvel.htm"
-#         )
-#     )
-# )
+    create_news(noticias)
+    return noticias
