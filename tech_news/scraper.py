3b674568598a16d56564e5b130b699a212dca5ed
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-07-08 12:08:49 -0300

    req 4.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -57,7 +57,8 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(".tec--list--lg .tec--btn--primary::attr(href)").get()
 
 
 # Requisito 5
