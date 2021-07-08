f2b636c8eee24c91480dc7405aebf669b6ceaff2
Carol Bezerra <carol.arbe@gmail.com>
2021-07-08 16:23:05 -0300

    Requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -54,15 +54,15 @@ def scrape_noticia(html_content):
 # Requisito 3
 def scrape_novidades(html_content):
     selector = Selector(text=html_content)
-    urls_list = selector.css(
+    return selector.css(
         ".tec--list__item article div h3 a::attr(href)"
     ).getall()
-    return urls_list
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(".tec--btn::attr(href)").get()
 
 
 # Requisito 5
