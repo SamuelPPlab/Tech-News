030cf981795743213be83424dcd2ff19e7e6986c
Carol Bezerra <carol.arbe@gmail.com>
2021-07-08 15:41:12 -0300

    Requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -53,7 +53,11 @@ def scrape_noticia(html_content):
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    urls_list = selector.css(
+        ".tec--list__item article div h3 a::attr(href)"
+    ).getall()
+    return urls_list
 
 
 # Requisito 4
