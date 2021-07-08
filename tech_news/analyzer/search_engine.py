b5a477f30f0e0dffde7d0bc5e93def0198d03858
andreserudo <andreserudo@gmail.com>
2021-07-08 00:39:02 -0400

    requisitos 6 e 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,29 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 6
+# https://docs.mongodb.com/manual/reference/operator/query/regex/
+# https://stackoverflow.com/questions/5925731/reorder-dictionary-in-python-according-to-a-list-of-values/52044835
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    items = search_news({'title': {'$regex': title, '$options': 'i'}})
+    matches = [(item['title'], item['url']) for item in items]
+    print(matches)
 
 
 # Requisito 7
+# https://stackoverflow.com/questions/22061723/regex-date-validation-for-yyyy-mm-dd/22061800
+# https://docs.python.org/3/library/re.html
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    rule = r'^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$'
+    match = re.search(rule, date)
+    if not match:
+        return 'Data inválida'
+    items = search_news({'timestamp': {'$regex': date, '$options': 'i'}})
+    matches = [(item['title'], item['url']) for item in items]
+    print(matches)
 
 
 # Requisito 8
