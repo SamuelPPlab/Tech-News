6acc66f786c22c6bf035f0a4a076ef6a6b379369
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-07-08 15:57:53 -0300

    requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -62,7 +62,8 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(".tec--list .tec--btn::attr(href)").get()
 
 
 # Requisito 5
