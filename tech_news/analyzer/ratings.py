a9949ec66e64dd38d7af0330685160dfc878c247
Felipe Nascimento <flpnascto@gmail.com>
2021-07-08 19:17:36 -0300

    requisito 10 e 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,39 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    pipeline = [
+        {
+            "$addFields": {
+                "popularidade": {
+                    "$sum": {"$add": ["$shares_count", "$comments_count"]}
+                }
+            }
+        },
+        {"$sort": {"popularidade": -1}},
+        {"$limit": 5},
+    ]
+    news_db = list(get_collection().aggregate(pipeline))
+    pprint.pprint(news_db)
+    news_list = []
+    for news in news_db:
+        news_list.append((news["title"], news["url"]))
+    return news_list
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    pipeline = [
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        # {"$limit": 5},
+    ]
+    news_db = get_collection().aggregate(pipeline)
+    news_list = []
+    for news in news_db:
+        news_list.append(news["_id"])
+    if len(news_list) > 5:
+        return sorted(news_list)[:5]
+    return sorted(news_list)
