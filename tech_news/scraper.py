3afafdcc897eb1ae6c164d905ffa7f8a624dc241
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-07-07 21:14:55 -0300

    tech_news_python

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+import time
+
+
+# python3 -m pytest tests/test_scraper.py
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code != 200:
+            return None
+        return response.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
