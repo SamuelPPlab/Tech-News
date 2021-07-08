9505eeeb59c7c75e0bae15a6ebe9e142f8b4652e
andreserudo <andreserudo@gmail.com>
2021-07-08 18:14:23 -0400

    requisito 7 - ajuste na data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -14,14 +14,16 @@ def search_by_title(title):
 
 # Requisito 7
 # https://stackoverflow.com/questions/22061723/regex-date-validation-for-yyyy-mm-dd/22061800
+# https://stackoverflow.com/questions/1711727/regular-expression-to-match-dates-in-yyyy-mm-dd-format
 # https://docs.python.org/3/library/re.html
+# https://docs.python.org/3/tutorial/errors.html
 def search_by_date(date):
     """Seu código deve vir aqui"""
-    rule = r'^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$'
-    match = re.search(rule, date)
-    if not match:
-        return 'Data inválida'
-    items = search_news({'timestamp': {'$regex': date, '$options': 'i'}})
+    rule = r'^20[0-2][0-9]-((0[1-9])|(1[0-2]))-([0-2][1-9]|3[0-1])$'
+    match = re.search(rule, date)    
+    if match is None:
+        raise ValueError('Data inválida')
+    items = search_news({'timestamp': {'$regex': date}})
     matches = [(item['title'], item['url']) for item in items]
     return matches
 
