8e259fa70313fa5975027942ac0b1b1f9d712167
sagacello <sagacello@hotmail.com>
2021-07-08 10:20:42 -0300

    requisito 10, nao importa nada

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,23 @@
 # Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    lista_ordenada = sorted(
+        [
+            (item["shares_count"] + item["comments_count"])
+            for item in find_news()
+        ]
+    )
+    return [
+        (f'noticia_{index + 1}',  item['url']) 
+        for index, item in enumerate(lista_ordenada) if index <= 4
+    ]
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+
+
+top_5_news()
