e5f8f4191ca48782f4e3b9822f9d21f90f4ec9fd
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-07-08 11:03:21 -0300

    Req 1 e 2.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,50 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    shares_count = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    comments_count = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    author = selector.css(".tec--author__info__link::text").get()
+    new_dict = {
+        "url": selector.css("meta[property='og:url']::attr(content)").get(),
+        "title": selector.css("#js-article-title::text").get(),
+        "timestamp": selector.css("time::attr(datetime)").get(),
+        "author": author.strip() if author else author,
+        "shares_count": int(shares_count) if shares_count else 0,
+        "comments_count": int(comments_count)
+        if comments_count
+        else comments_count,
+        "summary": "".join(
+            selector.css(".tec--article__body p:first-child *::text").getall()
+        ),
+        "sources": [
+            str(item).strip()
+            for item in selector.css("div.z--mb-16 .tec--badge::text").getall()
+        ],
+        "categories": [
+            str(item).strip()
+            for item in selector.css(
+                "div#js-categories a.tec--badge::text"
+            ).getall()
+        ],
+    }
+    return new_dict
 
 
 # Requisito 3
