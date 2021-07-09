ce2ac25faef51310dd4352bc7cb304bab1b1325a
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-07-09 10:02:01 -0300

    finalizando req 2 - 09/07/2021

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,28 +1,118 @@
 import requests
 import time
-
-
-LINK = "https://www.tecmundo.com.br/novidades"
+from parsel import Selector
 
 
 # Requisito 1
 def fetch(url):
     try:
-        resquest_url = requests.get(url, timeout=3)
         time.sleep(1)
-        print(resquest_url.status_code)
-        status = resquest_url.status_code
-        if status != 200:
+        RESPONSE = requests.get(url, timeout=3)
+        # print(RESPONSE.content)
+        STATUS = RESPONSE.status_code
+        if STATUS != 200:
             return None
-        html = resquest_url.text
-        return html
+        HTML = RESPONSE.text
+        return HTML
     except requests.Timeout:
         return None
 
 
+# link = "https://www.tecmundo.com.br/novidades"
+# fetch(link)
+
+
+def handle_shares_count(shares_count_list):
+    shares_count = 0
+    for count in shares_count_list:
+        # print(count.isnumeric())
+        if count.isnumeric() is True:
+            shares_count = int(count)
+    return shares_count
+
+
+def handle_sumary_elements(summary_list):
+    result = ""
+    for element in summary_list:
+        result += element
+    return result
+
+
+def remove_spaces(element):
+    element_list = element.split(" ")
+    new_element = ""
+    for item in element_list:
+        if item == element_list[len(element_list) - 2]:
+            new_element += item
+        elif (
+            item != element_list[0]
+            and item != element_list[len(element_list) - 1]
+        ):
+            new_element += item + " "
+    # print(new_element)
+    return new_element
+
+
+def handle_category_elements(category_list):
+    result = []
+    for element in category_list:
+        if element != " ":
+            new_element = remove_spaces(element=element)
+            result.append(new_element)
+    return result
+
+
+def handle_source_elements(source_list):
+    result = []
+    for element in source_list:
+        if element != " ":
+            new_element = remove_spaces(element=element)
+            result.append(new_element)
+    return result
+
+
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    SELECTOR = Selector(text=html_content)
+
+    url = SELECTOR.css("head link[rel=canonical]::attr(href)").get()
+    title = SELECTOR.css("h1.tec--article__header__title::text").get()
+    timestamp = SELECTOR.css("time::attr(datetime)").get()
+    writer_text = SELECTOR.css("a.tec--author__info__link::text").get()
+    writer = remove_spaces(writer_text)
+    shares_count_text = SELECTOR.css("div.tec--toolbar__item::text").get()
+    shares_count_list = shares_count_text.split(" ")
+    shares_count = handle_shares_count(shares_count_list=shares_count_list)
+    comments_count = int(
+        SELECTOR.css("button.tec--btn::attr(data-count)").get()
+    )
+    summary_element_list = SELECTOR.css(
+        "div.tec--article__body p:first-child *::text"
+    ).getall()
+    summary = handle_sumary_elements(summary_list=summary_element_list)
+    sources_list = SELECTOR.css("div.z--mb-16 div *::text").getall()
+    sources = handle_source_elements(source_list=sources_list)
+    categories_element_list = SELECTOR.css(
+        "div#js-categories *::text"
+    ).getall()
+    categories = handle_category_elements(categories_element_list)
+    dict_result = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    print(dict_result)
+    return dict_result
+
+
+# link = "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"
+# scrape_noticia(fetch(link))
 
 
 # Requisito 3
