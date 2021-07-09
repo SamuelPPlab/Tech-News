e4400fd7374b5a7fa022214f8d0238511792e6f3
CarolSi-hub <a.carolg1@gmail.com>
2021-07-09 16:51:25 -0300

    requisitos 2 3 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,7 @@
 import requests
 import time
+from parsel import Selector
+
 
 # src=https://docs.python-requests.org/en/master/user/quickstart/
 
@@ -19,17 +21,57 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    get_url = selector.css("head link[rel=canonical]::attr(href)").get()
+    get_title = selector.css("#js-article-title::text").get()
+    get_timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    get_writer = selector.css(".tec--author__info__link::text").get()
+    writer = get_writer.strip()
+    get_shares_count = selector.css(
+        "#js-author-bar > nav > div:nth-child(1)::text"
+    ).get()
+    suffix = " Compartilharam"
+    get_shares_count = get_shares_count[:-len(suffix)]
+    shares_count = int(get_shares_count) if get_shares_count else 0
+    get_comments_count = selector.css(
+        "#js-comments-btn::attr(data-count)").get()
+    comments_count = int(get_comments_count)
+    get_summary = selector.css(
+        ".tec--article__body p:nth-child(1) *::text"
+    ).getall()
+    summary = "".join(get_summary)
+    get_sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    sources = [source.strip() for source in get_sources]
+    get_categories = selector.css("#js-categories  a::text").getall()
+    categories = [category.strip() for category in get_categories]
+    dic_result = {
+        "url": get_url,
+        "title": get_title,
+        "timestamp": get_timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories
+    }
+    return dic_result
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url_list = selector.css(
+        "div.tec--card__info h3 a::attr(href)"
+    ).getall()
+    return url_list
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_link = selector.css("a.tec--btn.tec--btn--lg::attr(href)").get()
+    return next_link
 
 
 # Requisito 5
