c656acb9c5f32aba3507ec5f0d356578d3aca68d
alvarovascon <alvarovascon2@gmail.com>
2021-07-09 17:43:23 -0300

    req1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+
+    except requests.ReadTimeout:
+        return None
+
+    return response.text if response.status_code == 200 else None
 
 
 # Requisito 2
