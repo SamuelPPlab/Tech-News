2fba011d9a8c9abf41a272b9effb936810eaf811
Filipi Firmino <contatoic3web@gmail.com>
2021-07-09 13:06:10 -0300

    Challenge 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -67,7 +67,12 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    try:
+        selector = Selector(text=html_content)
+        next_page_link = selector.css(".tec--btn::attr(href)").get()
+        return next_page_link
+    except next_page_link.DoesNotExist:
+        return None
 
 
 # Requisito 5
