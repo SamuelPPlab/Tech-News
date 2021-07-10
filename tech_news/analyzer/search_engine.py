0c28a7eab41dde751ec9f4be32ecf4544f20532c
jr3564 <rodolfojr94@gmail.com>
2021-07-10 13:17:30 -0300

    Challenge 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,18 @@
 import tech_news.database as db
 import re
+from datetime import datetime
+
+
+def get_tuple_list(news_list):
+    return list(map(lambda news: (news["title"], news["url"]), news_list))
+
+
+def check_date(date):
+    try:
+        return datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
 
 # Requisito 6
 
@@ -8,16 +21,15 @@ def search_by_title(title):
     news_list = db.search_news(
         {"title": {"$regex": re.compile(title, re.IGNORECASE)}}
     )
-    news_tuple_list = list(
-        map(lambda news: (news["title"], news["url"]), news_list)
-    )
-
+    news_tuple_list = get_tuple_list(news_list)
     return news_tuple_list
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    check_date(date)
+    news_list = db.search_news({"timestamp": {"$regex": date}})
+    return get_tuple_list(news_list)
 
 
 # Requisito 8
