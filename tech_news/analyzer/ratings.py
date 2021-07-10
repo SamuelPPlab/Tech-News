47a6b7fb303e2695cb7566e11b116ce11442cf9c
alvarovascon <alvarovascon2@gmail.com>
2021-07-10 17:19:56 -0300

    req10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,14 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    for new in news:
+        new["popularity"] = new["comments_count"] + new["shares_count"]
+    sorted_news = sorted(news, key=lambda n: n["popularity"], reverse=True)
+    top_5 = [new for new in sorted_news[:5]]
+    return [(new["title"], new["url"]) for new in top_5]
 
 
 # Requisito 11
