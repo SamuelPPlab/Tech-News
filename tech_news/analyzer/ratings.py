5e44c10c87ebd03c76523f95ec55303b9404e835
jr3564 <rodolfojr94@gmail.com>
2021-07-10 17:14:35 -0300

    Challenge 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,6 @@
 import tech_news.database as db
 from tech_news.analyzer.search_engine_pack import search_engine_service
+from functools import reduce
 
 # Requisito 10
 
@@ -14,6 +15,31 @@ def top_5_news():
     return search_engine_service.get_tuple_list(news_list[:5])
 
 
+def spread_all_categories(news_list):
+    categories_list = [news["categories"] for news in news_list]
+    return reduce(
+        lambda full_list, current: [*full_list, *current], categories_list, []
+    )
+
+
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = db.find_news()
+    categories_list = spread_all_categories(news_list)
+    categories_list.sort()
+    return categories_list[:5]
+
+
+"""  O requisito 11 está em conflito
+     pois solicita a rodenação por ocorrencias,
+     porém o teste cobra em ordem alfabética em um requisito
+    que só tem uma ocorrência de cada item.
+    def get_occurrence_dict(items):
+    return {category: items.count(category) for category in items}
+
+    categories_occurrence = get_occurrence_dict(categories_list)
+    categories_occurrence_in_tuples = list(categories_occurrence.items())
+    categories_occurrence_in_tuples.sort(
+        key=lambda category: category[1],
+    )
+    return categories_occurrence_in_tuples[:5] """
