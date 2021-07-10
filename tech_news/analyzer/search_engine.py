f04c3af3c224dff0219631b3771db66db63be425
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-07-10 19:08:35 -0300

    finalizando req 6, 7, 8, 9 - 10/07/2021

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,47 @@
+from tech_news.database import search_news
+import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    search_list = search_news(query)
+    return [(new["title"], new["url"]) for new in search_list]
+
+
+# print(search_by_title("espanhol"))
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    format = "%Y-%m-%d"
+    try:
+        datetime.datetime.strptime(date, format)
+        query = {"timestamp": {"$regex": date, "$options": "i"}}
+        search_list = search_news(query)
+        return [(new["title"], new["url"]) for new in search_list]
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
+# print(search_by_date("2021-07-10"))
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    query = {"sources": {"$regex": source, "$options": "i"}}
+    search_list = search_news(query)
+    return [(new["title"], new["url"]) for new in search_list]
+
+
+print(search_by_source("Biel Glasses"))
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query = {"categories": {"$regex": category, "$options": "i"}}
+    search_list = search_news(query)
+    return [(new["title"], new["url"]) for new in search_list]
+
+
+print(search_by_category("Minha Série"))
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,6 @@ def fetch(url):
 
 
 def remove_spaces(element):
-    # print(element)
     if element is not None:
         element_list = element.split(" ")
         new_element = ""
@@ -37,18 +36,15 @@ def remove_spaces(element):
                 and item != element_list[len(element_list) - 1]
             ):
                 new_element += item + " "
-        # print(new_element)
         return new_element
     return element
 
 
 def handle_shares_count(shares_count_text):
-    # print(shares_count_text)
     if shares_count_text is not None:
         shares_count_list = shares_count_text.split(" ")
         shares_count = 0
         for count in shares_count_list:
-            # print(count.isnumeric())
             if count.isnumeric() is True:
                 shares_count = int(count)
         return shares_count
@@ -82,27 +78,15 @@ def handle_source_elements(source_list):
 
 def handle_writer_requests(SELECTOR):
     selector_list_css = [
-        # "#js-author-bar > div >"
-        # "p.z--m-none.z--truncate.z--font-bold > a::text",
-        # "#js-main > div.z--container > article > "
-        # "div.tec--article__body-grid > "
-        # "div.z--pt-40.z--pb-24 > div.z--flex.z--items-center > "
-        # "div.tec--timestamp.tec--timestamp--lg > "
-        # "div.tec--timestamp__item.z--font-bold > a::text",
-        # "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold > a::text",
-        # "#js-main > div.z--container > article > div.tec--article__body-grid > div.z--pt-40.z--pb-24 > div.z--flex.z--items-center > div.tec--timestamp.tec--timestamp--lg > div.tec--timestamp__item.z--font-bold > a::text",
-        # "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold > a::text",
-        # "#js-main > div.z--container > article > div.tec--article__body-grid > div.z--pt-40.z--pb-24 > div.z--flex.z--items-center > div.tec--timestamp.tec--timestamp--lg > div.tec--timestamp__item.z--font-bold > a::text",
-        # "#js-main > div.z--container > article > div.tec--article__body-grid > div.z--pt-40.z--pb-24 > div.z--flex.z--items-center > div.tec--timestamp.tec--timestamp--lg > div.tec--timestamp__item.z--font-bold > a::text"
-        # "#js-main > div.z--container > article > div.tec--article__body-grid > div.z--pt-40.z--pb-24 > div.z--flex.z--items-center > div.tec--timestamp.tec--timestamp--lg > div.tec--timestamp__item.z--font-bold > a::text",
-        ".tec--author__info__link::text"
+        "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold > "
+        "a::text",
+        ".tec--author__info__link::text",
     ]
     writer_text = None
 
     for select in selector_list_css:
         if writer_text is None:
             writer_text = SELECTOR.css(select).get()
-            # print("writer_text", writer_text)
 
     if writer_text is not None:
         return writer_text.strip()
@@ -112,14 +96,12 @@ def handle_writer_requests(SELECTOR):
 def handle_comments_count_requests(SELECTOR):
     selector_list_css = [
         "#js-comments-btn::attr(data-count)",
-        "div.tec--toolbar__item button#js-comments-btn::attr(data-count)",
     ]
     comments_count = None
 
     for select in selector_list_css:
         if comments_count is None:
             comments_count = SELECTOR.css(select).get()
-            # print("comments_count", comments_count)
 
     if comments_count is not None and comments_count.isnumeric() is True:
         return int(comments_count)
@@ -137,7 +119,6 @@ def handle_title_requests(SELECTOR):
     for select in selector_list_css:
         if title is None:
             title = SELECTOR.css(select).get()
-            # print("title", title)
 
     return title
 
@@ -147,33 +128,21 @@ def scrape_noticia(html_content):
     SELECTOR = Selector(text=html_content)
 
     url = SELECTOR.css("head link[rel=canonical]::attr(href)").get()
-
     title = handle_title_requests(SELECTOR=SELECTOR)
-
     timestamp = SELECTOR.css("time::attr(datetime)").get()
-
     writer = handle_writer_requests(SELECTOR=SELECTOR)
-
     shares_count_text = SELECTOR.css("div.tec--toolbar__item::text").get()
-
     shares_count = handle_shares_count(shares_count_text=shares_count_text)
-
     comments_count = handle_comments_count_requests(SELECTOR=SELECTOR)
-
     summary_element_list = SELECTOR.css(
         ".tec--article__body > p:first-child *::text"
     ).getall()
-
     summary = handle_sumary_elements(summary_list=summary_element_list)
-
     sources_list = SELECTOR.css("div.z--mb-16 div *::text").getall()
-
     sources = handle_source_elements(source_list=sources_list)
-
     categories_element_list = SELECTOR.css(
         "div#js-categories *::text"
     ).getall()
-
     categories = handle_category_elements(categories_element_list)
 
     dict_result = {
@@ -187,11 +156,11 @@ def scrape_noticia(html_content):
         "sources": sources,
         "categories": categories,
     }
-    # print(dict_result)
     return dict_result
 
 
-# link = "https://www.tecmundo.com.br/minha-serie/215302-she-hulk-renee-elise-goldsberry-entra-elenco-serie.htm"
+# link = "https://www.tecmundo.com.br/minha-serie/"
+# "215302-she-hulk-renee-elise-goldsberry-entra-elenco-serie.htm"
 # scrape_noticia(fetch(link))
 
 
@@ -200,7 +169,6 @@ def scrape_novidades(html_content):
     SELECTOR = Selector(text=html_content)
 
     link_list = SELECTOR.css("div.tec--card__info h3 a::attr(href)").getall()
-    # print(link_list)
     return link_list
 
 
@@ -222,41 +190,78 @@ def scrape_next_page_link(html_content):
 # scrape_next_page_link(fetch(link))
 
 
+def request_amount_less_twenty(html_base, amount, count_value, result_value):
+    print("amount menor que 20")
+    result = result_value
+    count = count_value
+    link_news_page = scrape_novidades(html_base)
+    for index in range(amount):
+        html_new = fetch(link_news_page[index])
+        dict_new = scrape_noticia(html_new)
+        result.append(dict_new)
+        count += 1
+        print(count, dict_new["title"], dict_new["writer"])
+    return {"result": result, "count": count}
+
+
+def request_number_of_news_less_twenty(
+    html_base, number_of_news, count_value, result_value
+):
+    print("number_of_news menor que 20")
+    result = result_value
+    count = count_value
+    link_news_page = scrape_novidades(html_base)
+    for index in range(number_of_news):
+        html_new = fetch(link_news_page[index])
+        dict_new = scrape_noticia(html_new)
+        result.append(dict_new)
+        count += 1
+        print(count, dict_new["title"], dict_new["writer"])
+    return {"result": result, "count": count}
+
+
+def request_amount_than_twenty(
+    html_base, number_of_news_value, count_value, result_value
+):
+    print("amount maior que 20")
+    result = result_value
+    count = count_value
+    number_of_news = number_of_news_value
+    link_news_page = scrape_novidades(html_base)
+    for link in link_news_page:
+        html_new = fetch(link)
+        dict_new = scrape_noticia(html_new)
+        result.append(dict_new)
+        number_of_news -= 1
+        count += 1
+        print(count, dict_new["title"], dict_new["writer"])
+    return {"result": result, "count": count, "number_of_news": number_of_news}
+
+
 def requests_loops_links_news(
     amount, html_base, result_value, news_number, count_value
 ):
     result = result_value
-    number_of_news = news_number
     count = count_value
+    number_of_news = news_number
 
     if amount <= 20 and html_base is not None:
-        print("amount menor que 20")
-        link_news_page = scrape_novidades(html_base)
-        for index in range(amount):
-            html_new = fetch(link_news_page[index])
-            dict_new = scrape_noticia(html_new)
-            result.append(dict_new)
-            count += 1
-            print(count, dict_new["title"], dict_new["writer"])
+        request = request_amount_less_twenty(html_base, amount, count, result)
+        result = request["result"]
+        count = request["count"]
     elif number_of_news <= 20 and html_base is not None:
-        print("number_of_news menor que 20")
-        link_news_page = scrape_novidades(html_base)
-        for index in range(number_of_news):
-            html_new = fetch(link_news_page[index])
-            dict_new = scrape_noticia(html_new)
-            result.append(dict_new)
-            count += 1
-            print(count, dict_new["title"], dict_new["writer"])
+        request = request_number_of_news_less_twenty(
+            html_base, number_of_news, count, result
+        )
+        result = request["result"]
+        count = request["count"]
     elif amount > 20 and html_base is not None:
-        print("amount maior que 20")
-        link_news_page = scrape_novidades(html_base)
-        for link in link_news_page:
-            html_new = fetch(link)
-            dict_new = scrape_noticia(html_new)
-            result.append(dict_new)
-            number_of_news -= 1
-            count += 1
-            print(count, dict_new["title"], dict_new["writer"])
+        request = request_amount_than_twenty(
+            html_base, number_of_news, count, result
+        )
+        result = request["result"]
+        count = request["count"]
+        number_of_news = request["number_of_news"]
     return {"result": result, "number_of_news": number_of_news}
 
 
@@ -276,9 +281,8 @@ def get_tech_news(amount):
         number_pages = math.ceil(amount / 20)
         number_of_news = amount
         for _ in range(number_pages):
-            # print("rodou")
-            print("LOOP PAGES")
-            print("NUMBER OF NEWS", number_of_news)
+            # print("LOOP PAGES")
+            # print("NUMBER OF NEWS", number_of_news)
             if number_of_news <= 20:
                 list_dicts = requests_loops_links_news(
                     amount, html_base, result, number_of_news, count
@@ -298,4 +302,4 @@ def get_tech_news(amount):
     return result
 
 
-# get_tech_news(30)
+# get_tech_news(2)
