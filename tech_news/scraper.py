c438d7b6e8d6c9f46e4d89713d00667014a1c70b
lisvalver <lisvalver.atte@gmail.com>
2021-07-09 21:58:52 -0300

    Req 04 - Create function scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -52,7 +52,10 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_link = selector.css(
+        ".tec--list > a::attr(href)").get()
+    return next_page_link
 
 
 # Requisito 5
