207c7f3545d52784f05685fcdcc6ef0d6ccfcdb5
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-07-10 19:36:30 -0300

    Requisito 2 e 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,20 @@
 import requests
 import time
-# from parsel import Selector
+from parsel import Selector
 
 
 # Requisito 1
 def fetch(url):
     try:
         response = requests.get(url, timeout=3)
-    except requests.ReadTimeout:
+    except (
+        requests.exceptions.RequestException,
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.Timeout,
+    ):
         return None
-    # response = requests.get(url)
-    # selector = Selector(text=response.text)
+
     time.sleep(1)
 
     if response.status_code != 200:
@@ -21,13 +25,63 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    noticia = {}
+    selector = Selector(text=html_content)
+
+    noticia["url"] = selector.css(
+        "meta[property='og:url']::attr(content)"
+    ).get()
+
+    noticia["title"] = selector.css("#js-article-title::text").get().strip()
+
+    noticia["timestamp"] = (
+        selector.css("#js-article-date ::attr(datetime)").get().strip()
+    )
+
+    if selector.css(".tec--author__info__link::text") is None:
+        noticia["writer"] = None
+    noticia["writer"] = (
+        selector.css(".tec--author__info__link::text").get().strip()
+    )
+
+    if selector.css(".tec--toolbar__item::text").get() is None:
+        noticia["shares_count"] = 0
+    noticia["shares_count"] = int(
+        selector.css(".tec--toolbar__item::text").get().strip()[:2]
+    )
+
+    if selector.css("#js-comments-btn::attr(data-count)").get() is None:
+        noticia["comments_count"] = 0
+    noticia["comments_count"] = int(
+        selector.css("#js-comments-btn::attr(data-count)").get()
+    )
+
+    noticia["summary"] = "".join(
+        selector.css(".tec--article__body > p:first-child *::text").getall()
+    )
+
+    noticia["sources"] = [
+        source.strip()
+        for source in selector.css(".z--mb-16 div a.tec--badge::text").getall()
+    ]
+
+    noticia["categories"] = [
+        category.strip()
+        for category in selector.css(
+            "#js-categories .tec--badge::text"
+        ).getall()
+    ]
+    return noticia
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
 
+    return selector.css("h3 .tec--card__title__link::attr(href)").getall()
+
+
+print(scrape_novidades(fetch("https://www.tecmundo.com.br/novidades")))
 
 # Requisito 4
 def scrape_next_page_link(html_content):
--- tech_news/tempCodeRunnerFile.py
+++ tech_news/tempCodeRunnerFile.py
@@ -1,28 +1,79 @@
 import requests
 import time
-
 from parsel import Selector
 
 
 # Requisito 1
 def fetch(url):
-
     try:
         response = requests.get(url, timeout=3)
-        print(1)
-    except requests.ReadTimeout:
-        print(2)
+    except (
+        requests.exceptions.RequestException,
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.Timeout,
+    ):
         return None
-    # response = requests.get(url)
-    selector = Selector(text=response.text)
-    # time.sleep(1)
+
+    time.sleep(1)
 
     if response.status_code != 200:
-        print(3)
         return None
 
-    print(4)
-    return print(selector.data)
+    return response.text
 
 
-fetch("https://app.betrybe.com/")
+# Requisito 2
+def scrape_noticia(html_content):
+    noticia = {}
+    selector = Selector(text=html_content)
+
+    noticia["url"] = selector.css(
+        "meta[property='og:url']::attr(content)"
+    ).get()
+
+    noticia["title"] = selector.css("#js-article-title::text").get().strip()
+
+    noticia["timestamp"] = selector.css(
+        "#js-article-date ::attr(datetime)"
+    ).get()
+
+    noticia["writer"] = (
+        selector.css(".tec--author__info__link::text").get().strip()
+    )
+
+    if selector.css(".tec--toolbar__item::text").get() is None:
+        noticia["shares_count"] = 0
+    noticia["shares_count"] = int(
+        selector.css(".tec--toolbar__item::text").get().strip()[0]
+    )
+
+    if selector.css("#js-comments-btn::attr(data-count)").get() is None:
+        noticia["comments_count"] = 0
+    noticia["comments_count"] = int(
+        selector.css("#js-comments-btn::attr(data-count)").get()
+    )
+
+    noticia["summary"] = "".join(
+        selector.css(".tec--article__body > p:first-child *::text").getall()
+    )
+
+    noticia["sources"] = [
+        source.strip()
+        for source in selector.css(".z--mb-16 div a.tec--badge::text").getall()
+    ]
+
+    noticia["categories"] = [
+        category.strip()
+        for category in selector.css(
+            "#js-categories .tec--badge::text"
+        ).getall()
+    ]
+    print(noticia)
+
+
+scrape_noticia(
+    fetch(
+        "https://www.tecmundo.com.br/dispositivos-moveis/215327-pixel-5a-tera-lancamento-limitado-devido-escassez-chips.htm"
+    )
+)
