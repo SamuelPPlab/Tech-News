8cda7fb06b84cf16643d0ebb4cc44ef27326b87e
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-07-10 14:41:39 -0300

    Adicionando requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 
 import time
 import requests
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,8 +18,43 @@ def fetch(url):
 
 
 # Requisito 2
+
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    all_news = {}
+    selector = Selector(text=html_content)
+
+    all_news['url'] = selector.css("link[ rel=canonical] ::attr(href)").get()
+
+    all_news['title'] = selector.css("#js-article-title::text").get()
+
+    all_news['timestamp'] = selector.css(
+        "#js-article-date ::attr(datetime)").get()
+
+    all_news['writer'] = (selector.css(
+        ".tec--author__info__link::text").get().strip()
+        if selector.css(".tec--author__info__link::text").get() else None)
+
+    all_news["shares_count"] = int(
+        selector.css(".tec--toolbar__item::text")
+        .get(default="0")
+        .replace("Compartilharam", "")
+        .strip()
+    )
+
+    all_news['comments_count'] = int(selector.css(
+        "#js-comments-btn ::attr(data-count)").get())
+
+    all_news['summary'] = "".join(selector.css(
+        ".tec--article__body > p:first-child *::text").getall())
+        
+    all_news['sources'] = list(map(str.strip, selector.css(
+        ".z--mb-16 .tec--badge::text").getall()))
+
+    all_news['categories'] = list(map(str.strip, selector.css(
+        "#js-categories a ::text").getall()))
+
+    return all_news
 
 
 # Requisito 3
