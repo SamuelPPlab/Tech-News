32880a2b7986969a488973e24c235b5de2c5175a
jr3564 <rodolfojr94@gmail.com>
2021-07-10 15:28:15 -0300

    Challenge 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+import tech_news.database as db
+from tech_news.analyzer.search_engine_pack import search_engine_service
+
 # Requisito 10
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = db.find_news()
+    news_list.sort(
+        key=lambda news: news["comments_count"] + news["shares_count"],
+        reverse=True,
+    )
+
+    return search_engine_service.get_tuple_list(news_list[:5])
 
 
 # Requisito 11
