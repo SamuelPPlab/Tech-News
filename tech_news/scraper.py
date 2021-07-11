b402b2fa6462e0438eb66a05a4762e9930291d10
samantabelow <samanta.below@gmail.com>
2021-07-11 15:54:10 -0300

    Requisitos 3 e 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -57,12 +57,14 @@ def scrape_noticia(html_content):
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css("h3 > .tec--card__title__link::attr(href)").getall()
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css(".tec--btn::attr(href)").get()
 
 
 # Requisito 5
