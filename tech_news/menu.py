abe4be6e67a2bf3ece67ee9d40715ef3737208de
jr3564 <rodolfojr94@gmail.com>
2021-07-11 15:51:25 -0300

    Challenge 12 ok

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -13,22 +13,21 @@ def generate_view_menu():
     exit_index = len(menu_options.options)
     print(f" {exit_index} - Sair.")
 
-    return exit_index
+    option_selected = int(input())
+
+    return option_selected if option_selected != exit_index else -1
 
 
 def analyzer_menu():
-    exit_index = generate_view_menu()
-
     try:
-        option_selected = int(input())
+        option_selected = generate_view_menu()
 
-        if option_selected == exit_index:
-            print("Encerrando script")
-            return
+        if option_selected == -1:
+            return print("Encerrando script")
 
         result = menu_options.options[option_selected][1]()
         print(result)
-    except ValueError or IndexError:
+    except ValueError:
         print("Opção inválida")
     except IndexError:
         print("Opção inválida", file=sys.stderr)
