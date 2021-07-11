28c2c6edd083b59fa35f54bdf6fde9bbcd4a856b
danielmlduarte <danielduarte.tryber@gmail.com>
2021-07-10 21:15:29 -0300

    Requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+import re
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    query = {"title": {"$regex": re.compile(title, re.IGNORECASE)}}
+    result = search_news(query)
+    news_found = [(news["title"], news["url"]) for news in result]
+    return news_found
 
 
 # Requisito 7
