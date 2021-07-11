7eeb1bba0e479ba8f2b3f7fcd9bb3689fa4dc45d
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-07-11 19:46:24 -0300

    req 1 e 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,54 @@
+import requests
+import time
+from parsel import Selector
+
 # Requisito 1
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("meta[property='og:url']::attr(content)").get()
+    title = selector.css("meta[property='og:title']::attr(content)").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = selector.css(".tec--author__info__link::text").get()
+    shares_count = selector.css(
+        "#js-author-bar > nav > div:nth-child(1)::text"
+    ).get()
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary = "".join(selector.css(
+            ".tec--article__body > p:first-child *::text"
+        ).getall()),
+    sources = list(map(
+            str.strip,
+            selector.css("div.z--mb-16 .tec--badge::text").getall(),
+        )),
+    categories = list(map(
+            str.strip,
+            selector.css("div#js-categories a.tec--badge::text").getall(),
+        )),
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer.strip() if writer is not None else None,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 3
