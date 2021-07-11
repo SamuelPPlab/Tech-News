3ca20e69bef96ef5dc84780e91fce95c18e70d74
samantabelow <samanta.below@gmail.com>
2021-07-11 18:35:11 -0300

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,20 +1,32 @@
 from tech_news.database import find_news
+import datetime
 
 
 # Requisito 6
 def search_by_title(title):
     title = title.lower()
     noticias = find_news()
-    noticias_com_title = [
+    return [
         (noticia["title"], noticia["url"])
         for noticia in noticias if title in noticia["title"].lower()
     ]
-    return noticias_com_title
 
 
 # Requisito 7
+def validate_date(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date(date)
+    noticias = find_news()
+    return [
+        (noticia["title"], noticia["url"])
+        for noticia in noticias if date == noticia["timestamp"][:10]
+    ]
 
 
 # Requisito 8
