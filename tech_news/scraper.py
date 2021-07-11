35fa4fd34a083c41d47bf583a96c4edb64179b39
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-07-11 20:28:02 -0300

    num deu certo =(

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,44 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css("time::attr(datetime)").get()
+    writer = selector.css("a.tec--author__info__link::text").get()
+    if writer:
+        writer.strip()
+    else:
+        writer = None
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    if shares_count:
+        shares_count = int((shares_count.strip()).split(" ")[0])
+    else:
+        shares_count = 0
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    if comments_count:
+        int(comments_count)
+    else:
+        comments_count = 0
+    summary = selector.css(
+        ".tec--article__body > p:first-child *::text").getall()
+    summary = "".join(summary)
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    sources = [source.strip() for source in sources]
+    categories = selector.css("#js-categories a::text").getall()
+    categories = [category.strip() for category in categories]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 3
