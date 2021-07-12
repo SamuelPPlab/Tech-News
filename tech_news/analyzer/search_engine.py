e41f6cd87212c237d56a67fad9097f937c4c4497
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-07-12 19:28:27 -0300

    7th req

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,5 @@
+# import re
+import datetime
 from tech_news.database import search_news
 
 
@@ -12,7 +14,29 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python
+    # https://www.kite.com/python/answers/how-to-validate-a-date-string-format-in-python
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        searched_by_date = search_news({"timestamp": {'$regex': date}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    news = []
+    for new in searched_by_date:
+        news.append((new['title'], new['url']))
+    return news
+    #         news = []
+    #         for new in searched_by_date:
+    #             news.append((new["title"], new["url"]))
+    #             return news
+    # try:
+    #     regex = r"^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$"
+    #     if re.match(regex, date) is not None:
+    #         searched_by_date = search_news({"timestamp": {'$regex': date}})
+    #     else:
+    #         return []
+    # except ValueError:
+    #     raise ValueError("Data inválida")
 
 
 # Requisito 8
