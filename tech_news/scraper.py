202f3051364dfddb451b1202435f2be21a0e1298
vini-lima <vini20.henrique@gmail.com>
2021-07-12 16:22:45 -0300

    Requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,8 +1,8 @@
 import requests
 import time
+from parsel import Selector
 
 
-# Requisito 1
 def fetch(url):
     try:
         time.sleep(1)
@@ -16,7 +16,42 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aquiiii"""
+    selector = Selector(html_content)
+    news = {}
+
+    news["url"] = selector.css('meta[property="og:url"]::attr(content)').get()
+
+    news["title"] = selector.css(
+        'meta[property="og:title"]::attr(content)').get()
+
+    news["timestamp"] = selector.css('time::attr(datetime)').get()
+
+    writer = selector.css('.tec--author__info__link::text').get()
+    if writer:
+        news["writer"] = writer.strip()
+    else:
+        news["writer"] = None
+
+    shares_count = selector.css('.tec--toolbar__item::text').get()
+    if shares_count:
+        news["shares_count"] = int((shares_count.strip()).split(" ")[0])
+    else:
+        news["shares_count"] = 0
+
+    news["comments_count"] = int(
+        selector.css('#js-comments-btn::attr(data-count)').get())
+
+    news["summary"] = "".join(
+        selector.css('.tec--article__body > p:first-child *::text').getall())
+
+    news["sources"] = [source.strip() for source in selector.css(
+        '.z--mb-16 div a.tec--badge::text').getall()]
+
+    news["categories"] = [categorie.strip() for categorie in selector.css(
+        '#js-categories .tec--badge::text').getall()]
+
+    print(news)
+    return news
 
 
 # Requisito 3
