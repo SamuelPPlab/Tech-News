bcefaf888c9b4e7b9276c419cbbd0d2d85e21e30
Filipi Firmino <contatoic3web@gmail.com>
2021-07-12 20:16:00 -0300

    Challenge 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import re
 
 
 def extract_data(data):
@@ -16,7 +17,14 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    regex_date = r'^20\d{2}-(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[01])$'
+    if (re.search(regex_date, date) is None):
+        raise ValueError("Data inválida")
+    response = search_news({'timestamp': {'$regex': date}})
+    result = []
+    for data in response:
+        result.append(extract_data(data))
+    return result
 
 
 # Requisito 8
