adbbe4e25befbdd1f580d6e76f4f15c165164886
rodrigoderkian <derkiandev@gmail.com>
2021-07-12 17:31:55 -0300

    req1/9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,53 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
+def format_news(new):
+    return ((new["title"], new["url"]))
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = []
+    # https://docs.mongodb.com/manual/reference/operator/query/regex/
+    search = {"title": {"$regex": title, "$options": "i"}}
+    result = search_news(search)
+    for new in result:
+        news.append(format_news(new))
+    return news
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news = []
+    format = "%Y-%m-%d"
+    try:
+        # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+        datetime.strptime(date, format)
+    except ValueError:
+        raise ValueError("Data inválida")
+    search = {"timestamp": {"$regex": date}}
+    result = search_news(search)
+    for new in result:
+        news.append(format_news(new))
+    return news
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = []
+    search = {"sources": {"$regex": source, "$options": "i"}}
+    result = search_news(search)
+    for new in result:
+        news.append(format_news(new))
+    return news
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = []
+    search = {"categories": {"$regex": category, "$options": "i"}}
+    result = search_news(search)
+    for new in result:
+        news.append(format_news(new))
+    return news
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,94 @@
+from tech_news.database import create_news
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    # https://qastack.com.br/programming/7524585/how-do-i-get-the-information-from-a-meta-tag-with-javascript
+    url = selector.css("meta[property='og:url']::attr(content)").get()
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css("time::attr(datetime)").get()
+    writer = selector.css(".tec--author__info__link::text").get()
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary = selector.css(
+        ".tec--article__body > p:first-child *::text"
+    ).getall()
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer.strip() if writer is not None else None,
+        "shares_count": int(
+            shares_count[:2].strip() if shares_count is not None else 0
+        ),
+        "comments_count": int(
+            comments_count.strip() if comments_count is not None else 0
+        ),
+        "summary": "".join(summary),
+        "sources": [source.strip() for source in sources],
+        "categories": [categorie.strip() for categorie in categories],
+    }
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    urls = selector.css("h3 .tec--card__title__link::attr(href)").getall()
+    return urls
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next = selector.css(".tec--btn--lg::attr(href)").get()
+    return next
 
 
-# Requisito 5
+# Requisito 5 (Fonte: Call com Anderson, Emerson e Zezo,  )
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    links = []
+    # lista de links
+    news = []
+    # lista de noticias
+    while len(links) < amount:
+        # a intenção aqui é utilizar o tamanho da lista de links para saber se
+        # é necessario ou não buscar novos links
+        content_html = fetch(url)
+        # buscando html da pagina principal
+        list_url = scrape_novidades(content_html)
+        # obtendo urls das noticias da pagina
+        url = scrape_next_page_link(content_html)
+        # alterando o valor de url (linha 67), para que na proxima interação
+        # do while, ele busque novos links
+        links += list_url
+        # adicionando os urls na lista de links, e uma vez
+        # que já possua mais links que a quantidade passada (amount), o laço
+        # se encerra
+
+    for list in links[:amount]:
+        # Tendo a lista de links, é feito um laço na lista de links
+        # utilizando somente a quantidade passada (amount), sacada do Anderson
+        news.append(scrape_noticia(fetch(list)))
+        # a noticia é adicionada a lista de noticias
+    create_news(news)
+    # a lista de noticias é salva no bd
+    return news
