fb61d99cc10ce5890bddcbe3dba997e6c99c8ab7
Filipi Firmino <contatoic3web@gmail.com>
2021-07-12 20:44:04 -0300

    challenge 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,27 @@
+from tech_news.database import find_news, get_collection
+
+
+def extract_data(data):
+    return(data['title'], data['url'])
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    response = find_news()
+    result = []
+    for data in response:
+        result.append(extract_data(data))
+    return result[:5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    response = list(get_collection())
+    result = []
+    for data in response:
+        result.append(data)
+    return result[:5]
+
+
+x = top_5_categories()
+print(x)
