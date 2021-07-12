2765022be1b2105329c6e834ad02acc145fa2d74
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-07-12 15:39:12 -0300

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    title_and_url_news = []
+    all_news = search_news(
+        {"title": {"$regex": f"{title}", "$options": "i"}}
+    )  # query @rafaelmguimaraes
+
+    for news in all_news:
+        title_and_url_news.append((news["title"], news["url"]))
+    return title_and_url_news
 
 
 # Requisito 7
