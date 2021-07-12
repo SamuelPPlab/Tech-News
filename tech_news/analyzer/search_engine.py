e5c4fc1284cb814193be7ee0dc3bc09fa42b9674
Filipi Firmino <contatoic3web@gmail.com>
2021-07-12 19:48:35 -0300

    Request 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+
+
+def extract_data(data):
+    return(data['title'], data['url'])
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    response = search_news({'title': {'$regex': title, '$options': 'i'}})
+    result = []
+    for data in response:
+        result.append(extract_data(data))
+    return result
 
 
 # Requisito 7
@@ -16,3 +27,7 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# result = search_by_title("Vamoscomtudo")
+# print(result)
