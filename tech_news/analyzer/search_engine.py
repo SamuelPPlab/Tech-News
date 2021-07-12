d28701910a7008e586869073813bbbb77ca1b0f1
Macarena Rodriguez <macarenar697@gmail.com>
2021-07-12 14:52:35 -0300

    Req 07 - Function search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,10 +1,16 @@
-from tech_news.database import search_news
+from tech_news.database import search_news, get_collection
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
     title = title.lower().capitalize()
     response = search_news({"title": title})
+    tupla_response = response_treatment(response)
+    return tupla_response
+
+
+def response_treatment(response):
     if response:
         tupla_response = [(response[0].get("title"), response[0].get("url"))]
     else:
@@ -14,7 +20,26 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        # year, month, day = date.split("-")
+        # datetime(int(year), int(month), int(day))
+        datetime.strptime(date, "%Y-%m-%d")
+        initial_date_time = date + "T00:00:00.000Z"
+        finally_date_time = date + "T24:59:59.000Z"
+        response = list(
+            get_collection().find(
+                {
+                    "timestamp": {
+                        "$gte": (initial_date_time),
+                        "$lte": (finally_date_time),
+                    }
+                }
+            )
+        )
+        tupla_response = response_treatment(response)
+        return tupla_response
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
