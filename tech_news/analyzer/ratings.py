3712e516eb58c2f00bb8ad9f416e645141642b14
brendalima <brendalima@>
2021-07-12 17:39:04 -0300

    Sets a ranking of top 5 news by popularity

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    top = []
+    for new in news:
+        new['popularity'] = new['shares_count'] + new['comments_count']
+    news = sorted(news, key=lambda x: x['title'])
+    news = sorted(news, key=lambda x: x['popularity'], reverse=True)
+
+    for new in news[:5]:
+        top.append((new['title'], new['url']))
+
+    return top
 
 
 # Requisito 11
