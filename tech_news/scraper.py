c23cb000a56e98d7d972e893ac43dfd19ea538c4
Camila Valentim <camilaifpb@hotmail.com>
2021-07-12 16:22:53 -0300

    requisitos 2,3 e 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -18,16 +19,57 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = (
+        selector.css(".tec--author__info__link::text").get().strip()
+        if selector.css(".tec--author__info__link::text").get() else None
+    )
+    shares_count = int(selector.css(".tec--toolbar__item::text").get()[1:2])
+    comments_count = int(selector.css(".tec--btn::attr(data-count)").get())
+    summary = "".join(selector.css(
+        ".tec--article__body > p:first-child *::text"
+    ).getall())
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    for i in range(len(sources)):
+        sources[i] = sources[i].strip()
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+    for i in range(len(categories)):
+        categories[i] = categories[i].strip()
+    return {
+      "url": url,
+      "title": title,
+      "timestamp": timestamp,
+      "writer": writer,
+      "shares_count": shares_count,
+      "comments_count": comments_count,
+      "summary": summary,
+      "sources": sources,
+      "categories": categories,
+    }
 
 
 # Requisito 3
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    lista = selector.css(".tec--card__info h3 a::attr(href)").getall()
+    return lista
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    lista = (
+        selector.css(".tec--btn::attr(href)").get()
+        if selector.css(".tec--btn::attr(href)").get()
+        else None
+    )
+
+    return lista
 
 
 # Requisito 5
