cad3ebfb35037ee1ffe144dce0c589127cb26ece
brendalima <brendalima@>
2021-07-12 16:56:05 -0300

    Allows to search news by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
@@ -13,7 +14,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        search_results = search_news({"timestamp": {"$regex": date}})
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    list = []
+    for result in search_results:
+        list.append((result['title'], result['url']))
+
+    return list
 
 
 # Requisito 8
