ae433959cbe1ff10c9b635e27ed1192e8339aac7
brendalima <brendalima@>
2021-07-11 23:06:23 -0300

    Structuring news scraper

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import time
 import requests
 from requests.exceptions import HTTPError, ReadTimeout
+from parsel import Selector
 
 
 # Requisito 1
@@ -21,7 +22,30 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css("#js-article-title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    author = selector.css(".tec--author__info__link::text").get().strip()
+    shares = selector.css(".tec--toolbar__item::text").get().split()[0]
+    shares_count = int(shares) if shares else 0
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary = selector.css(
+        ".tec--article__body p:nth-child(1) *::text"
+    ).getall()
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": author,
+        "shares_count": shares_count,
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 3
