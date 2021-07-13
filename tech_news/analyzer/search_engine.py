217300c3a4eaf2e9cc03a1c4d6aa9c6448c86e64
lucas-andrad <lafdesouza2002@outlook.com>
2021-07-12 21:28:05 -0300

    Requisito 6, 8 e 9 \o/

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+# from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    result = search_news({'title': {'$regex': title, '$options': 'i'}})
+    list = []
+    for x in result:
+        touple = (x['title'], x['url'])
+        list.append(touple)
+    return list
 
 
 # Requisito 7
@@ -11,8 +21,22 @@ def search_by_date(date):
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    result = search_news({'sources': {'$elemMatch': {
+        '$regex': source, '$options': 'i'}}})
+    list = []
+    for x in result:
+        touple = (x['title'], x['url'])
+        list.append(touple)
+    return list
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    result = search_news({'categories': {'$elemMatch': {
+        '$regex': category, '$options': 'i'}}})
+    list = []
+    for x in result:
+        touple = (x['title'], x['url'])
+        list.append(touple)
+    return list
