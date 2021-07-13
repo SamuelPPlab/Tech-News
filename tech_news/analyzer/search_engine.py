d5bdc8b3c10324d9c53a5e8ac3a532f25533ff23
gabrielluiz-tec <gabrielsilva.adm@gmail.com>
2021-07-13 13:38:26 -0300

    req 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,5 @@
 import datetime
-from tech_news.database import find_news
+from tech_news.database import find_news, search_news
 
 
 # Requisito 6
@@ -17,10 +17,9 @@ def search_by_title(title):
 def search_by_date(date):
     try:
         datetime.datetime.strptime(date, '%Y-%m-%d')
-        find_date = find_news({"timestamp": {"$regex": date}})
+        find_date = search_news({"timestamp": {"$regex": date}})
     except ValueError:
         raise ValueError("Data inválida")
-
     mylist = []
     for index in find_date:
         mylist.append((index['title'], index['url']))
@@ -29,9 +28,25 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    try:
+        find_source = search_news({
+            "sources": {"$regex": source, "$options": "i"}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    mylist = []
+    for index in find_source:
+        mylist.append((index['title'], index['url']))
+    return mylist
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    try:
+        find_category = search_news({
+            "categories": {"$regex": category, "$options": "i"}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    mylist = []
+    for index in find_category:
+        mylist.append((index['title'], index['url']))
+    return mylist
