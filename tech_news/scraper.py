331f09bc405da7579233a76ca85bd0c3d47b55db
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-07-13 06:34:52 -0300

    Inicio req 2 e req 3 feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,3 +1,4 @@
+from parsel import Selector
 import requests
 import time
 
@@ -16,12 +17,33 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    data = {}
+    data["url"] = selector.css(
+        "head link[rel=canonical]::attr(href)"
+    ).get()
+    data["title"] = selector.css(
+        ".tec--article__header__title::text"
+    ).get()
+    data["timestamp"] = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+    data["writer"] = (
+        selector.css(".tec--author__info__link::text").get().strip()
+    )
+    data["shares_count"] = (
+        selector.css(".tec--toolbar__item svg").get()
+    )
+    print(data)
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    links = selector.css(
+        ".tec--list__item .tec--card_thumb_link::attr(href)"
+    ).getall()
+    return links
 
 
 # Requisito 4
