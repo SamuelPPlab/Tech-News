ac5112b84e01e49087147d6e5bae7e1c719fc67b
Kesley Lahoz <namielahoz@gmail.com>
2021-07-13 20:07:58 -0300

    trying passing 3 requirements

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,16 +18,49 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css("#js-article-title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    author = selector.css(".tec--author__info__link::text").get().strip()
+    shares = selector.css(".tec--toolbar__item::text").get().split()[0]
+    shares_count = int(shares) if shares else 0
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary = selector.css(
+        ".tec--article__body p:nth-child(1) *::text"
+    ).getall()
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": author,
+        "shares_count": shares_count,
+        "comments_count": int(comments_count),
+        "summary": "".join(summary),
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    data = selector.css(
+        ".tec--list__item .tec--card__thumb__link::attr(href)"
+    ).getall()
+    if len(data) == 0:
+        return []
+    else:
+        return data
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next = selector.css(".tec--btn::attr(href)").get()
+    return next
 
 
 # Requisito 5
