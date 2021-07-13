b70ac91477e4ac9225ad7c0281166c8b48d3dba6
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-07-13 17:35:12 -0300

    10th req

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+from tech_news.database import find_news
+from operator import itemgetter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    try:
+        found_news = find_news()
+        news = sorted(found_news, key=itemgetter("title"))
+        if (len(news) <= 0):
+            return news
+        if not news:
+            return None
+    except AssertionError:
+        return None
+    most_popular_news = sorted(
+        news,
+        key=lambda item: item["shares_count"] + item["comments_count"],
+        reverse=True,
+    )[:5]
+    return [
+        (most_popular_news["title"], most_popular_news["url"])
+        for most_popular_news in most_popular_news
+    ]
 
 
 # Requisito 11
