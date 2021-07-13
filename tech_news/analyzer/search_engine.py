d1980984bd9e7bb11f992c6ca6bda36b696d9dc8
gabrielluiz-tec <gabrielsilva.adm@gmail.com>
2021-07-13 11:19:03 -0300

    req 6 e 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,29 @@
+import datetime
+from tech_news.database import find_news
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    all_news = find_news()
+    result = []
+
+    for news in all_news:
+        if title.lower() in news['title'].lower():
+            result.append((news['title'], news['url']))
+    return result
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        find_date = find_news({"timestamp": {"$regex": date}})
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    mylist = []
+    for index in find_date:
+        mylist.append((index['title'], index['url']))
+    return mylist
 
 
 # Requisito 8
