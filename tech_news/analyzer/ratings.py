2bf3b1e86e1ad9dd1d290d0b0958641aa928edd9
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-07-13 19:27:31 -0300

    requisitos 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    all_news = find_news()
+    top_five_news = []
+    for news in all_news:
+        news["total_rate"] = news["shares_count"] + news["comments_count"]
+    rated_news = sorted(all_news, key=lambda keys: ("total_rate", "title"))[:5]
+    
+    for item in rated_news:
+        top_five_news.append((item["title"], item["url"]))
+
+    print(top_five_news)
+    return top_five_news
 
 
 # Requisito 11
