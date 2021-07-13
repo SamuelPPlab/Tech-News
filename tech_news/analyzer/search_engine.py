c8c1b720e155a2bac9ff2ac65131ab8d59b36499
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-07-12 23:00:58 -0300

    6,7,8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,36 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = []
+    for news in search_news({"title": {"$regex": title, "$options": "i"}}):
+        tupla = (news['title'], news['url'])
+        news_list.append(tupla)
+    return news_list
 
 
-# Requisito 7
+# Requisito 7 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+        news_list = []
+        for news in search_news({"timestamp": {"$regex": date, "$options": "x"}}):
+            tupla = (news['title'], news['url'])
+            news_list.append(tupla)
+        return news_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news_list = []
+    for news in search_news({"sources": {"$regex": source, "$options": "i"}}):
+        tupla = (news['title'], news['url'])
+        news_list.append(tupla)
+    return news_list
 
 
 # Requisito 9
