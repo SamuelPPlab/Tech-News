59491368322d88332bbbab5ef3554bcaafd752e0
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-07-14 08:06:52 -0300

    requisito 7 e 8 -v3

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,15 +2,15 @@ from tech_news.database import search_news
 import datetime
 
 
-def insensitive(title):
-    query = {"title": {"$regex": title, "$options": "i"}}
+def insensitive(item, attribute):
+    query = {f"{attribute}": {"$regex": item, "$options": "i"}}
     return query
 
 
 # Requisito 6
 def search_by_title(title):
     titles = []
-    query = insensitive(title)
+    query = insensitive(title, "title")
     response_list = search_news(query)
     for news in response_list:
         titles.append((news["title"], news["url"]))
@@ -21,19 +21,18 @@ def search_by_title(title):
 # Requisito 7
 # https://stackoverflow.com/questions/4709652/python-regex-to-match-dates
 def search_by_date(date):
-    datetime.datetime.strptime(date, "%y-%m-%d")
-    response_news_by_date = search_news({"timestamp": {"$regex": date}})
-
-    if (response_news_by_date is None):
+    try:
+        datetime.datetime.strptime(date, "%y-%m-%d")
+        response_news_by_date = search_news({"timestamp": {"$regex": date}})
+        news = [(new['title'], new['url']) for new in response_news_by_date]
+        return news
+    except ValueError:
         raise ValueError("Data inválida")
-
-    news = [(new['title'], new['url']) for new in response_news_by_date]
-    return news
 
 
 # Requisito 8
 def search_by_source(source):
-    query = insensitive(source)
+    query = insensitive(source, "sources")
     response_list = search_news(query)
     news = [(new['title'], new['url']) for new in response_list]
     return news
