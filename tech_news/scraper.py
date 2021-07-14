fbcbfb278953a3fdb09be80dce3bb849967abfc1
CIANDT\brune <brune.freire@bradesco.com.br>
2021-07-13 21:10:06 -0300

    Iniciando o projeto tech news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,16 @@
+import requests
+import time
+from tech_news.database import create_news
+from requests.exceptions import ReadTimeout
+from parsel import Selector
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 2
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 3
-def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 4
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 5
-def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+    except ReadTimeout:
+        return None
