8200f82d8a9324e6820ec1f4224e19cb917ae9e9
Thays Costa <thayscosta3@gmail.com>
2021-07-14 17:27:09 -0300

    Req. 2 - scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 import requests
 import time
 from requests.exceptions import ReadTimeout
+from parsel import Selector
+import html2text
 
 
 # Requisito 1
@@ -17,7 +19,38 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    """Recebe um conteúdo HTML e retorna um dict com as informações solicitadas"""
+    noticia = {}
+    selector = Selector(html_content)
+    noticia["url"] = selector.css("link[rel='canonical']::attr(href)").get()
+    noticia["title"] = selector.css("#js-article-title::text").get()
+    noticia["timestamp"] = selector.css("time::attr(datetime)").get()
+
+    writer = selector.css(".tec--author__info__link::text").get().strip()
+    noticia["writer"] = None if not writer else writer
+
+    shares_count = int(selector.css(
+        ".tec--toolbar__item::text"
+    ).get().split()[0])
+    noticia["shares_count"] = 0 if not shares_count else shares_count
+
+    noticia["comments_count"] = int(selector.css(
+        "#js-comments-btn::attr(data-count)"
+    ).get())
+
+    first_paragraph = selector.css(".tec--article__body p").get()
+    convert_html2text = html2text.HTML2Text()
+    convert_html2text.ignore_links = True
+    summary = convert_html2text.handle(first_paragraph).replace("\n", " ")
+    noticia["summary"] = summary.replace("  ", "")
+    # ref: https://github.com/Alir3z4/html2text/blob/master/docs/usage.md
+
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    noticia["sources"] = [source.strip() for source in sources]
+
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+    noticia["categories"] = [category.strip() for category in categories]
+    return noticia
 
 
 # Requisito 3
