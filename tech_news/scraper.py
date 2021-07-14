46c686e9541a2e989aa15da5adff1df38bbce2fc
CIANDT\brune <brune.freire@bradesco.com.br>
2021-07-13 21:44:19 -0300

    Corrigindo requisito 2 e 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -22,22 +22,32 @@ def scrape_noticia(html_content):
 
 
     selector = Selector(html_content)
-    writer = selector.css(".tec--author__info__link::text").get()
-    categories = selector.css("#js-categories .tec--badge::text").getall()
-    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
-    shares = selector.css(".tec--toolbar__item::text").re_first()
-    comments = selector.css("#js-comments-btn::attr(data-count)").get()
+    url = selector.css("meta[property='og:url']::attr(content)").get()
+    title = selector.css('.tec--article__header__title::text').get()
+    timestamp = selector.css('.tec--timestamp--lg time::attr(datetime)').get()
+    writer = selector.css('.tec--author__info__link::text').get()
+    shares_count = selector.css('.tec--toolbar__item::text').get()
+    comments_count = selector.css('#js-comments-btn::attr(data-count)').get()
+    summary = selector.css(
+        '.tec--article__body > p:first-child *::text'
+    ).getall()
+    sources = selector.css('.z--mb-16 .tec--badge::text').getall()
+    categories = selector.css('#js-categories .tec--badge::text').getall()
+
     return {
-        "url": selector.css("meta[property='og:url']::attr(content)").get(),
-        "title": selector.css(".tec--article__header__title::text").get(),
-        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
-        "writer": writer.strip() if writer else None,
-        "shares_count": int(shares) if shares else 0,
-        "comments_count": int(comments) if comments else 0,
-        "summary": "".join(selector.css
-        (".tec--article__body > p:first-child *::text").getall()),
-        "sources": [source.strip() for source in sources],
-        "categories": [categorie.strip() for categorie in categories],
+        'url': url,
+        'title': title,
+        'timestamp': timestamp,
+        'writer': writer.strip() if writer is not None else None,
+        'shares_count':
+            int(shares_count.strip().split(' ')[0])
+            if shares_count is not None else 0,
+        'comments_count':
+            int(comments_count)
+            if comments_count is not None else 0,
+        'summary': ''.join(summary),
+        'sources': [source.strip() for source in sources],
+        'categories': [category.strip() for category in categories],
     }
 
 
