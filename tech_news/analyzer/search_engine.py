34f6dc9c9e3f7f74620a340c970706509d9b5603
PedroMarqDev <PedroMarqDev@gmail.com>
2021-07-14 01:27:06 -0300

    Mongo requisitos finalizados

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,39 @@
+from tech_news.database import search_news
+import datetime
+
+# Baseado no código do Arthur Massaini sobre como
+# utilizar o regex em datas e importação do datetime.
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    paginatedList = search_news(
+        {"title": {"$regex": f"{title}", "$options": "i"}})
+    return [(news["title"], news["url"]) for news in paginatedList]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    dateFormat = "%Y-%m-%d"
+    try:
+        datetime.datetime.strptime(date, dateFormat)  # Parse here
+    except ValueError:
+        raise ValueError("Data inválida")
+    finally:
+        paginatedList = search_news(
+            {"timestamp": {"$regex": f"{date}", "$options": "i"}})
+        return [(news["title"], news["url"]) for news in paginatedList]
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    paginatedList = search_news(
+        {"sources": {"$regex": f"{source}", "$options": "i"}})
+    return [(news["title"], news["url"]) for news in paginatedList]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    paginatedList = search_news(
+        {"categories": {"$regex": f"{category}", "$options": "i"}})
+    return [(news["title"], news["url"]) for news in paginatedList]
