82793f4203b6616b42b3b2277a928e7293f005ed
danilocouto1 <dansantosalfa@gmail.com>
2021-07-15 19:00:59 -0300

    Requisistos de  6 - 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,46 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    requested_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    if len(requested_news) > 0:
+        return [(requested_news[0]["title"], requested_news[0]["url"])]
+    else:
+        return []
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        news_by_date = search_news({"timestamp": {"$regex": date}})
+        if len(news_by_date) > 0:
+            return [(news_by_date[0]["title"], news_by_date[0]["url"])]
+        else:
+            return []
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news_by_source = search_news(
+        {"sources": {"$regex": source, "$options": "i"}}
+    )
+    if len(news_by_source) > 0:
+        return [(news_by_source[0]["title"], news_by_source[0]["url"])]
+    else:
+        return []
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_by_category = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+    if len(news_by_category) > 0:
+        return [(news_by_category[0]["title"], news_by_category[0]["url"])]
+    else:
+        return []
