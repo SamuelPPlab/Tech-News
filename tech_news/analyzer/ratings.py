d8960de5bc3aed90323456ed4317c639eead4d1e
Lorena Góes <goesloren@gmail.com>
2021-07-16 16:07:07 -0300

    Challenges 10 and 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,36 @@
+from tech_news.database import find_news
+from functools import reduce
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    info = list()
+    news = find_news()
+    for news_item in news:
+        rate = news_item["shares_count"]
+        + news_item["comments_count"]
+        get_tuple = (
+            news_item["title"],
+            news_item["url"], rate
+        )
+        info.append(get_tuple)
+    info.sort(key=lambda item: item[2], reverse=True)
+    # const teste = (item) => item == item[2]
+    result = [(item[0], item[1]) for item in info]
+    return result[:5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    info = [news_item["categories"] for news_item in news]
+    result = reduce(
+        lambda news_list, current: [*news_list, *current],
+        info, [])
+    # counter = {category: result.count(category) for category in result}
+    counter = list()
+    for category in result:
+        counter.append((category, result.count(category)))
+    counter.sort(key=lambda item: item[0])  # Organiza em order alfabética
+    counter.sort(key=lambda item: item[1], reverse=True)
+    return [element[0] for element in counter][:5]
