addb53dfab39e670f79383593ec509968b8993d1
MoisesSantana <moisaant@gmail.com>
2021-07-17 18:40:39 -0300

    req 01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,21 @@
+import requests
+import time
+
+# from parsel import Selector
+# URL = 'https://www.tecmundo.com.br/novidades'
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
