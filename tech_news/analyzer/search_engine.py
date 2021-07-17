5ec7b5767fcbe9bb4137d7ac39fda0773dfdea12
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-07-17 19:40:31 -0300

    req 7 to 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,67 @@
+from tech_news.database import search_news
+from datetime import datetime
 # Requisito 6
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query_search = {'title': {'$regex': title, '$options': 'i'}}
+    news_by_title = search_news(query_search)
+    news_all = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        news_all.append((title, url))
+    return news_all
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query_search = {
+        "timestamp": {
+            "$gte": date+"T00:00:00",
+            "$lte": date+"T23:59:59",
+        }
+    }
+    news_by_title = search_news(query_search)
+    news_all = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        news_all.append((title, url))
+    return news_all
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    query_search = {"sources": {
+        "$elemMatch": {"$regex": source, "$options": "i"}}
+    }
+    news_by_title = search_news(query_search)
+    news_all = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        news_all.append((title, url))
+    return news_all
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query_search = {"categories": {
+        "$elemMatch": {"$regex": category, "$options": "i"}}
+    }
+    news_by_title = search_news(query_search)
+    news_all = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        news_all.append((title, url))
+    return news_all
+
+if __name__ == "__main__":
+    search_by_title("batman")
