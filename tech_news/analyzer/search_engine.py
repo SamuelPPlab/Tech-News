6de12a7495ffc4fb7ce87d9e1f7aca3525b4b987
Thays Costa <thayscosta3@gmail.com>
2021-07-17 21:49:11 -0300

    Req. 06 - busca por título de notícias

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """ - Recebe uma string, faz pesquisa case-insentive por títulos de notícias e:
+        - Retorna a notícia caso haja correspondência e uma lista vazia se não.
+    """
+    # Ref: Bruna Sepulveda + encurtador.com.br/bpLQ2
+
+    get_news = search_news({"title": {"$regex": title, "$options": "i"}})
+
+    return [
+        (news["title"], news["url"])
+        for news in get_news
+    ]
 
 
 # Requisito 7
@@ -16,3 +29,7 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+teste = 'CAIXA_ALTA'
+print(teste.lower())
