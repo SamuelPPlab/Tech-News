013352912d6f42b4790d8a33d2f23291e2e595b4
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-07-18 18:11:48 -0300

    feat: requirement 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,8 +1,10 @@
 import requests
 import time
+
 # import math
 
-# from parsel import Selector
+from parsel import Selector
+
 # from tech_news.database import create_news
 
 
@@ -20,12 +22,66 @@ def fetch(url):
 
     except requests.Timeout:  # acima de 3 segundos Timeout
         return None
+
+
 # Vitor Rodrigues
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+
+    selector = Selector(text=html_content)
+
+    scraped_url = selector.css("head link[rel=canonical]::attr(href)").get()
+
+    scraped_title = selector.css("h1.tec--article__header__title::text").get()
+
+    scraped_timestamp = selector.css(
+        "div.tec--timestamp__item time::attr(datetime)"
+    ).get()
+
+    scraped_writer = selector.css("a.tec--author__info__link::text").get()
+
+    scraped_source = selector.css(".z--mb-16 .tec--badge::text").getall()
+
+    scraped_categories = selector.css("div#js-categories a::text").getall()
+
+    if scraped_writer:
+        scraped_writer = scraped_writer.strip()
+    scraped_shares_count = selector.css("div.tec--toolbar__item::text").get()
+
+    if scraped_shares_count:
+        scraped_shares_count = int(
+            (scraped_shares_count.strip()).split(" ")[0]
+        )
+    else:
+        scraped_shares_count = 0
+    scraped_comments_count = selector.css("div.tec--toolbar__item::text").get()
+
+    if scraped_comments_count:
+        scraped_comments_count = int(
+            (scraped_comments_count.strip()).split(" ")[0]
+        )
+    else:
+        scraped_comments_count = 0
+    scraped_summary = selector.css(
+        ".tec--article__body p:first-child *::text"
+    ).getall()
+
+    return {
+        "url": scraped_url,
+        "title": scraped_title,
+        "timestamp": scraped_timestamp,
+        "writer": scraped_writer,
+        "shares_count": scraped_shares_count,
+        "comments_count": scraped_comments_count,
+        "summary": "".join(scraped_summary),
+        "sources": [source.strip() for source in scraped_source],
+        "categories": [category.strip() for category in scraped_categories],
+    }
+
+
+# Vanessa Bidinoto
 
 
 # Requisito 3
