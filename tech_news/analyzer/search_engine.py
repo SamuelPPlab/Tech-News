74e85d56db9c884b4c0a489afdfaac2cdab2aabe
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-07-18 21:58:02 -0300

    req 8 e 9 completos

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -16,7 +16,7 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     try:
-        datetime.datetime.strptime(date, '%Y-%m-%d')
+        datetime.datetime.strptime(date, "%Y-%m-%d")
         result_search = search_news({"timestamp": {"$regex": date}})
         if result_search:
             result_one = result_search[0]
@@ -25,17 +25,33 @@ def search_by_date(date):
         else:
             return []
     except ValueError:
-        raise ValueError('Data inválida')
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    result_search = search_news(
+        {"sources": {"$regex": source, "$options": "i"}}
+    )
+    if result_search:
+        result_one = result_search[0]
+        tupla_search = [(result_one["title"], result_one["url"])]
+        return tupla_search
+    else:
+        return []
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    result_search = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+    if result_search:
+        result_one = result_search[0]
+        tupla_search = [(result_one["title"], result_one["url"])]
+        return tupla_search
+    else:
+        return []
 
 
-search_by_date("2020-11-23")
+search_by_source("Venture Beat")
