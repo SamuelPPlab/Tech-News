f24138c8fb425fc87d44a3675fb4a40298a7631e
mateus <mateuslaalmeida@gmail.com>
2021-07-20 13:59:57 -0300

    Requisitos 1-10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    responses = find_news()
+    list_sorted = sorted(
+        responses,
+        key=lambda response: response["shares_count"]
+        + response["comments_count"],
+        reverse=True,
+    )
+    list = []
+    limit = slice(5)
+    for item in list_sorted:
+        list.append((item["title"], item["url"]))
+    return list[limit]
 
 
 # Requisito 11
--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,48 @@
+import re
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    title_formated = re.compile(re.escape(title), re.IGNORECASE)
+    responses = search_news({"title": title_formated})
+    list = []
+    for response in responses:
+        list.append((response["title"], response["url"]))
+    return list
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date != datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        date_formated = re.compile(re.escape(date), re.IGNORECASE)
+        responses = search_news({"timestamp": date_formated})
+        list = []
+        for response in responses:
+            list.append((response["title"], response["url"]))
+    return list
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    source_formated = re.compile(re.escape(source), re.IGNORECASE)
+    responses = search_news({"sources": source_formated})
+    list = []
+    for response in responses:
+        list.append((response["title"], response["url"]))
+    return list
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    category_formated = re.compile(re.escape(category), re.IGNORECASE)
+    responses = search_news({"categories": category_formated})
+    list = []
+    for response in responses:
+        list.append((response["title"], response["url"]))
+    return list
