7f37c98e30cffaa01670a23d29fac067e2850281
mateus <mateuslaalmeida@gmail.com>
2021-07-21 12:18:32 -0300

    Requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,6 @@
 from tech_news.database import find_news
+from collections import Counter
+import itertools
 
 
 # Requisito 10
@@ -19,4 +21,13 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    responses = find_news()
+    categories_list = []
+    for response in responses:
+        categories_list.append(response["categories"])
+    totals = Counter(
+        i for i in list(itertools.chain.from_iterable(categories_list))
+    )
+    totals_sorted = sorted(totals)
+    limit = slice(5)
+    return totals_sorted[limit]
