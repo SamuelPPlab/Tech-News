4d3e2b0f0250a83be0f9c03bbbadd851db1fdb78
Bruno Nogueira <nugobruno@gmail.com>
2021-07-21 08:21:57 -0300

    req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import search_news
 import re
+import datetime
 
 
 # Requisito 6
@@ -14,23 +15,39 @@ def search_by_title(title):
     return list_of_news
 
 
+def test_valid_date(date):
+    # Ref: https://www.codevscolor.com/date-valid-check-python#:~:
+    # text=Python%20Program%20%3A&text=split('%2F')%20isValidDate,
+    # is%20not%20valid..%22)&text=If%20its%20value%20is%20'True,
+    # date%2C%20else%20it%20is%20not.
+    year, month, day = date.split('-')
+
+    try:
+        datetime.datetime(int(year), int(month), int(day))
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 # Requisito 7
 def search_by_date(date):
     regex = re.compile(r'^\d{4}-\d{2}-\d{2}$')
     # Ref: https://stackoverflow.com/questions/1711727/
     # regular-expression-to-match-dates-in-yyyy-mm-dd-format
+
+    test_valid_date(date)
+
     try:
-        regex.match(date)
+        valid_format = bool(regex.match(date))
+        if valid_format:
+            result = search_news({'timestamp': {'$regex': date}})
+            list_of_news = []
+
+            for new in result:
+                list_of_news.append((new['title'], new['url']))
+            return list_of_news
 
     except ValueError:
         raise ValueError("Data inválida")
-
-    result = search_news({'timestamp': {'$regex': date}})
-    list_of_news = []
-
-    for new in result:
-        list_of_news.append((new['title'], new['url']))
-    return list_of_news
 
 
 # Requisito 8
