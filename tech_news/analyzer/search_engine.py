d02a391f54c56ea3dc4b5482a0c5f44d7cd87f7b
Bruno Nogueira <nugobruno@gmail.com>
2021-07-21 08:02:23 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,36 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    result = search_news({'title': {'$regex': title, '$options': 'i'}})
+    # Referência: projetod a Thays Costa
+    list_of_news = []
+
+    for new in result:
+        list_of_news.append((new['title'], new['url']))
+
+    return list_of_news
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    regex = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+    # Ref: https://stackoverflow.com/questions/1711727/
+    # regular-expression-to-match-dates-in-yyyy-mm-dd-format
+    try:
+        regex.match(date)
+
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    result = search_news({'timestamp': {'$regex': date}})
+    list_of_news = []
+
+    for new in result:
+        list_of_news.append((new['title'], new['url']))
+    return list_of_news
 
 
 # Requisito 8
