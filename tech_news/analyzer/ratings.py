1809848570f8af4672cf21f130f2e4398631ab83
Thays Costa <thayscosta3@gmail.com>
2021-07-22 18:03:42 -0300

    Req. 11 - Top 5 categorias de notícias

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,7 +1,8 @@
 from tech_news.database import find_news
+import collections
+from functools import reduce
 
 
-# Requisito 10
 def top_5_news():
     """
         - Pesquisa no DB e retorna as 5 notícias mais populares;
@@ -27,4 +28,23 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    """
+        - Pesquisa no DB e retorna as 5 categorias mais recorrentes;
+            - Ordena resultados alfabeticamente;
+            - Havendo menos de 5 categorias, retorna todas
+            - Se não houver categorias, retorna lista vazia
+    """
+    # Ref. feito com ajuda da Lorena Goes e Rodolfo Martins
+    get_all_news = find_news()
+    categories = [news["categories"] for news in get_all_news]
+    all_categories = reduce(
+        lambda categories_list, current: [*categories_list, *current],
+        categories, [])
+    repeat_counting = list(collections.Counter(all_categories).items())
+    repeat_counting.sort(key=lambda category: category[0])
+    #  ordena alfebeticamente
+    repeat_counting.sort(key=lambda category: category[1], reverse=True)
+    #  ordena pela frequência
+
+    pop_categories = [info[0] for info in repeat_counting]
+    return pop_categories[:5]
