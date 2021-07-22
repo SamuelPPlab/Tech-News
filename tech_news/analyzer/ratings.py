2046efbc187bd0eaf5a955af59bd3eb9c4fd9c02
Raphael Caputo <faelelf2@gmail.com>
2021-07-22 20:47:12 -0300

    Finalizandoooooo

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,44 @@
+from tech_news.database import find_news
+from operator import itemgetter
+from collections import Counter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    """Retorna as 5 mais populares"""
+    allNews = find_news()
+
+    listPopular = sorted(
+        allNews,
+        key=lambda news: news["shares_count"] + news["comments_count"],
+        reverse=True,
+    )
+
+    listByName = sorted(listPopular, key=itemgetter("title"))
+
+    titleUri = []
+
+    for noticia in listByName:
+        titleUri.insert(len(titleUri), (noticia["title"], noticia["url"]))
+    return titleUri[:5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    """5 noticias mais populares por categoria"""
+
+    news = find_news()
+    listCategories = []
+    categorieValue = []
+
+    for n in news:
+        for c in n["categories"]:
+            listCategories.append(c)
+
+    countCategories = Counter(listCategories).most_common()
+    result = sorted(countCategories)
+
+    for r in result:
+        categorieValue.append(r[0])
+
+    return categorieValue[:5]
--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,77 @@
-# Requisito 12
+import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_category,
+    search_by_date,
+    search_by_source,
+)
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+
+
+def index0():
+    totalNews = input("Digite quantas notícias serão buscadas: ")
+    return get_tech_news(totalNews)
+
+
+def index1():
+    title = input("Digite o título da notícia: ")
+    return search_by_title(title)
+
+
+def index2():
+    date = input("Digite a data da notícia: ")
+    return search_by_date(date)
+
+
+def index3():
+    source = input("Digite a fonte: ")
+    return search_by_source(source)
+
+
+def index4():
+    category = input("Digite a categoria: ")
+    return search_by_category(category)
+
+
+def index5():
+    return top_5_news()
+
+
+def index6():
+    return top_5_categories()
+
+
+def index7():
+    return sys.exit()
+
+
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    index = input(
+        "Selecione uma das opções a seguir:\n"
+        + " 0 - Popular o banco com notícias;\n"
+        + " 1 - Buscar notícias por título;\n"
+        + " 2 - Buscar notícias por data;\n"
+        + " 3 - Buscar notícias por fonte;\n"
+        + " 4 - Buscar notícias por categoria;\n"
+        + " 5 - Listar top 5 notícias;\n"
+        + " 6 - Listar top 5 categorias;\n"
+        + " 7 - Sair.\n"
+    )
+    choice = {
+        "0": index0,
+        "1": index1,
+        "2": index2,
+        "3": index3,
+        "4": index4,
+        "5": index5,
+        "6": index6,
+        "7": index7,
+    }
+
+    try:
+        result = choice[index]()
+        print(result)
+        return result
+    except KeyError:
+        print("Opção inválida")
