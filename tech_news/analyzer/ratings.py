c85a2d8e5c7fb6058bd10967661eb5f326c7ecb5
MoisesSantana <moisaant@gmail.com>
2021-07-22 19:23:11 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,28 @@
+from tech_news.database import find_news
+
+
+# https://stackoverflow.com/questions/72899/how-do-i-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary
+# Mario F e Boris
+# Gleison e Carol
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    response = find_news()
+
+    if len(response) == 0:
+        return response
+
+    news_list_with_order = []
+    for news in response:
+        news['order'] = int(news['shares_count']) + int(news['comments_count'])
+        news_list_with_order.append(news)
+
+    sorted_list = sorted(
+        news_list_with_order, key=lambda curr_key: (
+            curr_key['title'], curr_key['order']
+        )
+    )[:5]
+
+    return [(news['title'], news['url']) for news in sorted_list]
 
 
 # Requisito 11
