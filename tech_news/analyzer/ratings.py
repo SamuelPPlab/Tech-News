0fd33c4881e631375683525eabfd175781d53c6f
Thays Costa <thayscosta3@gmail.com>
2021-07-21 23:20:29 -0300

    Req. 5 - Pesquisa top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,28 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    """
+        - Pesquisa no DB e retorna as 5 notícias mais populares;
+            - Calcula popularidade pela soma de compartilhamentos e comentários
+            - Ordena resultados alfabeticamente por título;
+            - Havendo menos de 5 notícias, retorna quantas tiver
+    """
+    get_all_news = find_news()
+    news_info = [
+       (
+           news["title"], news["url"],
+           news["shares_count"] + news["comments_count"]
+        )
+       for news in get_all_news
+    ]
+    sorted(news_info, key=lambda news: news[0], reverse=True)
+
+    news_by_popularity = [
+        (info[0], info[1]) for info in news_info
+    ]
+    return news_by_popularity[:5]
 
 
 # Requisito 11
