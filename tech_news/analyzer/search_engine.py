1196b0c93b2806301bf69908c1bf9383bb04dd72
MoisesSantana <moisaant@gmail.com>
2021-07-22 17:12:34 -0300

    req 06

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,22 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    result = []
+    param = title.lower()
+    param = param.capitalize()
+    response = search_news({"title": param})
+
+    if len(response) == 0:
+        return result
+
+    dict_response = response[0]
+    response_title = dict_response['title']
+    response_url = dict_response['url']
+    tuple_response = (response_title, response_url)
+    result.append(tuple_response)
+    return result
 
 
 # Requisito 7
