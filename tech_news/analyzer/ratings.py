0ad74b58f2e82e6e47c2513367680f1eea759d69
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-07-23 19:52:14 -0300

    feat: requirement 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,32 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_found = find_news()
+
+    news_popularity = [
+        (news['shares_count'] + news['comments_count'],
+            news['title'], news['url'])
+        for news in news_found
+    ]
+
+    news_popularity_sorted = sorted(news_popularity, key=lambda var: var[1])
+    news_popularity_sorted = sorted(
+        news_popularity, key=lambda var: var[0], reverse=True)
+
+    top_five_categories = []
+
+    for news in news_popularity_sorted:
+        news_keys = (news[1], news[2])
+        top_five_categories.append(news_keys)
+
+    return top_five_categories[:5]
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+
+
+
