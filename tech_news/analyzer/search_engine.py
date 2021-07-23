2e8deb2b0273107db92f21878267756440478ddb
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-07-23 11:00:13 -0300

    fix: bugs

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,29 +1,55 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
     query = {"title": {"$regex": title, "$options": "i"}}
-    titles_list = search_news(query)
+    found_results = search_news(query)
+    news_by_title = []
 
-    news_list = []
+    for news in found_results:
+        news_by_title.append((news["title"], news["url"]))
 
-    for news in titles_list:
-        news_list.append((news["title"], news["url"]))
-
-    return news_list
+    return news_by_title
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        query = {"timestamp":  {"$regex": date}}
+        found_results = search_news(query)
+        news_by_date = []
+
+        for news in found_results:
+            news_keys = (news["title"], news["url"])
+            news_by_date.append(news_keys)
+        return news_by_date
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    query = {"sources":  {"$regex": source, "$options": "i"}}
+    found_results = search_news(query)
+    news_by_source = []
+
+    for news in found_results:
+        news_keys = (news["title"], news["url"])
+        news_by_source.append(news_keys)
+    return news_by_source
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query = {"categories":  {"$regex": category, "$options": "i"}}
+    found_results = search_news(query)
+    news_by_category = []
+
+    for news in found_results:
+        news_keys = (news["title"], news["url"])
+        news_by_category.append(news_keys)
+    return news_by_category
