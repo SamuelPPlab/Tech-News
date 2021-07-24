960a0a1840b58bb015397e6a5d67b06760d995e5
Lets <letsferreiratx@gmail.com>
2021-07-24 11:36:43 -0300

    req 2 e 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -23,8 +23,22 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    writer = selector.css(".tec--author__info__link::text").get()
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    news = {}
+    news["url"] = selector.css("head link[rel=canonical]::attr(href)").get()
+    news["title"] = selector.css("head > meta[property='og:title']::attr(content)").get()
+    news["timestamp"] = selector.css("#js-article-date::attr(datetime)").get()
+    news["writer"] = writer.strip() if writer is not None else None
+    news["shares_count"] = int(shares_count[: -len("Compartilharam")].strip()) if shares_count is not None else 0
+    news["comments_count"] = int(comments_count) if comments_count is not None  else 0
+    news["summary"] = "".join(selector.css(".tec--article__body > p:first-child *::text").getall())
+    news["sources"] = [source.strip() for source in selector.css(".z--mb-16 div a.tec--badge::text").getall()]
+    news["categories"] = [category.strip() for category in selector.css("#js-categories .tec--badge::text").getall()]
 
+    return news
 
 # Requisito 3
 def scrape_novidades(html_content):
@@ -34,7 +48,8 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(".tec--btn--lg::attr(href)").get()
 
 
 # Requisito 5
