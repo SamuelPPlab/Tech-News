2dbb587ffb1214690d46126b68fdc11de5a074da
vitorAmorims <vtamorims@gmail.com>
2021-07-25 18:16:58 -0300

    logica para o menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,6 @@
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    criar uma lista com as opcoes do menu
+    loop no menu e capturar a opcao escolhida
+    print na opção com o numero escolhidos
+    
