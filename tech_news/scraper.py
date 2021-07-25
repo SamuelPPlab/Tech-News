7637d23c420cb86ee7308e1182aac3c6cde64f38
LAURO DE SA <itrafor@gmail.com>
2021-07-25 16:02:17 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+import time
+from parsel import Selector
+from requests.exceptions import ReadTimeout
+
+url = "https://www.tecmundo.com.br/novidades"
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+    except ReadTimeout:
+        None
 
 
 # Requisito 2
