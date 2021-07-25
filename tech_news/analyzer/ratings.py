8c1034f72b4fa8f3d4c6ba2bd78ce94542a4387b
vicmafe <victormateuslf@gmail.com>
2021-07-25 12:48:39 -0300

    def top five news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    for new in news:
+        new["popularity"] = new["comments_count"] + new["shares_count"]
+
+    classify_news = sorted(news, key=lambda n: n["popularity"], reverse=True)
+    five_selected_news = [new for new in classify_news[:5]]
+    top_news_five = [(new["title"], new["url"]) for new in five_selected_news]
+
+    return top_news_five
 
 
 # Requisito 11
