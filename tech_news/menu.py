e405490813a3fc90ad320f9be7e548587496de7c
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-07-26 20:31:29 -0300

    req 12

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,74 @@
+import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_source,
+    search_by_category,
+)
+
+MENU_OPTIONS = (
+    "Selecione uma das opções a seguir:\n"
+    + " 0 - Popular o banco com notícias;\n"
+    + " 1 - Buscar notícias por título;\n"
+    + " 2 - Buscar notícias por data;\n"
+    + " 3 - Buscar notícias por fonte;\n"
+    + " 4 - Buscar notícias por categoria;\n"
+    + " 5 - Listar top 5 notícias;\n"
+    + " 6 - Listar top 5 categorias;\n"
+    + " 7 - Sair.\n"
+)
+
+
+def option_zero():
+    message = input("Digite quantas notícias serão buscadas: ")
+    response = get_tech_news(int(message))
+    print(response)
+
+
+def option_one():
+    message = input("Digite o título: ")
+    response = search_by_title(message)
+    print(response)
+
+
+def option_two():
+    message = input("Digite a data no formato aaaa-mm-dd: ")
+    response = search_by_date(message)
+    print(response)
+
+
+def option_three():
+    message = input("Digite a fonte: ")
+    response = search_by_source(message)
+    print(response)
+
+
+def option_four():
+    message = input("Digite a categoria: ")
+    response = search_by_category(message)
+    print(response)
+
+
+def option_five():
+    response = top_5_news()
+    print(response)
+
+
+def option_six():
+    response = top_5_categories()
+    print(response)
+
+
+def option_seven():
+    print("Encerrando script")
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    option = input(MENU_OPTIONS)
+
+    if not option.isdigit() or int(option) not in range(8):
+        print("Opção inválida", file=sys.stderr)
+        return
