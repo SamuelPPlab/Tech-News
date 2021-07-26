e4f060ce18f924663fc6a6fdd8d70d3b925fc847
SamuelPPlab <samuelpp.s@hotmail.com>
2021-07-26 19:18:49 -0300

    req 6 pronto

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import get_collection
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    
+    cursor = get_collection().find({ "title": { "$regex": title, "$options": "i" } }, { "_id": False, "title": True, "url": True })
+    result = []
+    for item in cursor:
+        result.append((item["title"], item["url"]))
+    return result
+
 
 
 # Requisito 7
