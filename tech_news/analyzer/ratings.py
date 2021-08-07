967e5113a63520d87c6c59e6ea297daf7971db65
LAURO DE SA <itrafor@gmail.com>
2021-08-07 14:42:23 -0300

    Requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,6 @@
 # Requisito 10
 from tech_news.database import find_news
+from functools import reduce
 
 
 def top_5_news():
@@ -14,6 +15,18 @@ def top_5_news():
     result = [(item[0], item[1]) for item in info]
     return result[:5]
 
+
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    news = find_news()
+    info = [news_item["categories"] for news_item in news]
+    result = reduce(
+        lambda news_list, current: [*news_list, *current], info, []
+    )
+    counter = list()
+    for category in result:
+        counter.append((category, result.count(category)))
+    counter.sort(key=lambda item: item[0])
+    counter.sort(key=lambda item: item[1], reverse=True)
+    return [element[0] for element in counter][:5]
