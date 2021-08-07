d01ece1c3f1a96e3d44437039e22389e8b21012f
LAURO DE SA <itrafor@gmail.com>
2021-08-07 13:33:27 -0300

    Requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
 # Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+
+    get_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    info = list()
+    for news_item in get_news:
+        if news_item["title"]:
+            get_tuple = (news_item["title"], news_item["url"])
+            info.append(get_tuple)
+    return info
 
 
 # Requisito 7
