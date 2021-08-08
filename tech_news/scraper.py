5f6ec2c57bbbbf24d7e75bc0b403dddab645490d
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-08 10:30:49 -0300

    req1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,31 @@
-# Requisito 1
+import requests
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+    except requests.Timeout:
+        return None
+    finally:
+        if (response.status_code == 200):
+            return response.headers["Content-Type"]
+        else:
+            return None
 
 
-# Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
 
 
-# Requisito 3
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
 
 
-# Requisito 4
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
 
 
-# Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
