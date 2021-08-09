055aeaac079637b0a02c34eea801a8924a9266af
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-09 20:03:48 -0300

    eu disse amem ?

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,43 @@
-# Requisito 10
+from tech_news.database import get_collection
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    db = get_collection().aggregate(
+        [
+            {
+                "$project": {
+                    "_id": 0,
+                    "title": 1,
+                    "url": 1,
+                    "popularidade": {
+                        "$add": ["$comments_count", "$shares_count"]
+                    },
+                }
+            },
+            {"$limit": 5},
+        ]
+    )
+
+    result = []
+    for item in db:
+        result.append((item["title"], item["url"]))
+    return result
 
 
-# Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    db = get_collection().aggregate(
+        [
+            {"$unwind": "$categories"},
+            {
+                "$group": {
+                    "_id": "$categories",
+                }
+            },
+            {"$sort": {"_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    result = []
+    for item in db:
+        result.append(item["_id"])
+    return result
