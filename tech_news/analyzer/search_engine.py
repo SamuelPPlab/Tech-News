d426fd3fabd4c876430c3cb169ffbf952df3f47d
Eriquy Jhordan <eriquy@gmail.com>
2021-08-10 11:16:13 -0300

    search engine

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,59 @@
-# Requisito 6
+from tech_news.database import search_news
+from datetime import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search = {'title': {'$regex': title, '$options': 'i'}}
+    news_by_title = search_news(search)
+    all_news = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        all_news.append((title, url))
+    return all_news
 
 
-# Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Data inválida")
+    query_search = {
+        "timestamp": {
+            "$gte": date+"T00:00:00",
+            "$lte": date+"T23:59:59",
+        }
+    }
+    news_by_title = search_news(query_search)
+    all_news = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        all_news.append((title, url))
+    return all_news
 
 
-# Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    query_search = {"sources": {
+        "$elemMatch": {"$regex": source, "$options": "i"}}
+    }
+    news_by_title = search_news(query_search)
+    all_news = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        all_news.append((title, url))
+    return all_news
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query_search = {"categories": {
+        "$elemMatch": {"$regex": category, "$options": "i"}}
+    }
+    news_by_title = search_news(query_search)
+    all_news = []
+    for news in news_by_title:
+        title = news.get("title")
+        url = news.get("url")
+        all_news.append((title, url))
+    return all_news
